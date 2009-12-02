fa41045eeeef0f4602dd09e96c8e54a7a4d428ac
Marcin Kościelnicki <koriakin@0x04.net>
2009-12-02 22:24:16 +0000

    ctx: First serious look.
    Added relevant PGRAPH register info, plus some instruction descriptions.
    
    Did you know that Fermi will have 32 context-switching microcontrollers
    running in parallel, all of them able to run arbitrary C++ code?

--- ctxdis.c
+++ ctxdis.c
@@ -31,6 +31,59 @@
 #include <stdint.h>
 #include <unistd.h>
 
+/*
+ * PGRAPH registers of interest
+ *
+ * 0x400300: some sort of status register... shows 0xe when execution stopped,
+ *           0xX3 when running or paused.
+ * 0x400304: control register. For writes:
+ *           bit 0: start execution
+ *           bit 1: resume execution
+ *           bit 4: pause execution
+ *           For reads:
+ *           bit 0: prog running or paused
+ * 0x400308: Next opcode to execute, or the exit opcode if program exitted.
+ * 0x40030c: RO alias of PC for some reason
+ * 0x400310: PC. Readable and writable. In units of single insns.
+ * 0x40031c: the scratch register, written by opcode 2, holding params to
+ *           various stuff.
+ * 0x400324: code upload index, WO. selects address to write in ctxprog code,
+ *           counted in whole insns.
+ * 0x400328: code upload, WO. writes given insn to code segment and
+ *           autoincrements upload address.
+ * 0x400334: $r register, offset in RAMIN context to read/write with opcode 1.
+ *           in units of 32-bit words.
+ * 0x400338: Some register, set with opcode 3. Used on NVAx with values 0-9 to
+ *           select TP number to read/write with G[0x8000]. Maybe generic
+ *           PGRAPH offset? Also set to 0x8ffff in one place for some reason.
+ * 0x40033c: Some other register. Set with opcode 0x600007. Related to and
+ *           modified by opcode 8. Setting with opcode 0x600007 always ands
+ *           contents with 0xffff8 for some reason.
+ * 0x400824-0x400830: Flags, see below.
+ *
+ * How execution works:
+ *
+ * Execution can be in one of 3 states: stopped, running, and paused.
+ * Initially, it's stopped. The only way to start execution is to poke 0x400304
+ * with bit 0 set. When you do this, the uc starts executing opcodes from
+ * current PC. The only way to stop execution is to hit opcode 0x60000c.
+ * Note that 0x60000c resets PC to 0, so ctxprog will start over from there
+ * next time, unless you poke PC before that. Running <-> paused transition
+ * happens by poking 0x400304 with the appropriate bit set to 1. Poking with
+ * *both* bits set to 1 causes it to reload the opcode register from current
+ * PC, but nothing else happens. This can be used to read the code segment.
+ * Poking with bits 4 and 0 set together causes it to start with paused state.
+ *
+ * Oh, also, PC has 9 valid bits. So you can have ctxprogs of max. 512 insns.
+ */
+
+/*
+ * All of the above has been checked on my NV86 laptop card and will be
+ * checked soon on my NVA5. Should probably apply to all NV50 family cards.
+ * NV40 is rather different, but should be somewhat similar, needs to be
+ * checked some day.
+ */
+
 #define NV4x 1
 #define NV5x 2 /* and 8x and 9x and Ax */
 #define NVxx 3
@@ -188,7 +241,7 @@ int flagoff[] = { 0, 7, 0 };
 int gsize4off[] = { 14, 6, 0 };
 int gsize5off[] = { 16, 4, 0 };
 int immoff[] = { 0, 20, 0 };
-int goffoff[] = { 0, 16, 11 };
+int goffoff[] = { 0, 20, 0 };
 #define UNIT atomnum, unitoff
 #define FLAG atomnum, flagoff
 #define GSIZE4 atomnum, gsize4off
@@ -210,9 +263,9 @@ void atomreg APROTO {
 	fprintf (out, " %s$%s", cbl, (char *)v);
 }
 #define RA R("a")	// The scratch register. 0x40031c
-#define RG R("g")	// PGRAPH offset register.
+#define RG R("g")	// PGRAPH offset register [?]. 0x400338
 #define RR R("r")	// RAMIN offset register. 0x400334
-#define RM R("m")	// ??? 0x40033c. always aligned to 8.
+#define RM R("m")	// ??? 0x40033c, writing from ctxprog aligns to 8.
 
 /*
  * Memory fields
@@ -228,16 +281,32 @@ int pgmem5[] = { 0, 16, 2, 'G' };
 void atommem APROTO {
 	const int *n = v;
 	fprintf (out, " %s%c[", ccy, n[3]);
-	if (n[3] == 'G') printf("%s$g%s+", cbl, ccy);
+//	if (n[3] == 'G') printf("%s$g%s+", cbl, ccy);
 	int mo = BF(n[0], n[1])<<n[2];
 	fprintf (out, "%s%#x%s]", cyel, mo, ccy);
 }
 
+/*
+ * The flags, used as predicates for jump and wait insns.
+ *
+ * There are 0x80 of them, mapping directly to bits in PGRAPH regs 0x824
+ * through 0x830. Known ones:
+ *
+ *  - 0x824 bit 0 [0x00]: direction flag used by pgraph opcode to select
+ *    transfer direction.
+ *  - 0x824 and 0x828, remaining bits [0x01-0x3f]: ??? RW
+ *  - 0x82c [0x40-0x5f]: RO, some sort of PGRAPH status or something...
+ *  - 0x82c bit 13 [0x4d]: always set, used for unconditional jumps
+ *  - 0x830 bits 0-10 [0x60-0x69]: copied straight from relevant bits in PMC
+ *    0x1540, the TP enable bits.
+ *
+ * None of this checked on NV40.
+ */
 struct insn tabrpred[] = {
 	{ NVxx, 0x00, 0x7f, N("dir") }, // the direction flag
 	{ NV5x, 0x4d, 0x7f },	// always?
 	{ NV5x, 0x60, 0x60, N("unit"), UNIT }, // if given unit present
-	{ NVxx, 0x00, 0x00, N("flag"), FLAG }, // if given flag set in PGRAPH+0x824
+	{ NVxx, 0x00, 0x00, N("flag"), FLAG },
 };
 
 struct insn tabpred[] = {
@@ -250,18 +319,18 @@ struct insn tabm[] = {
 	{ NV5x, 0x100000, 0xf00000, N("pgraph"), PGRAPH5, GSIZE5 },
 	{ NV4x, 0x100000, 0xffc000, N("pgraph"), PGRAPH4, RA },
 	{ NV4x, 0x100000, 0xf00000, N("pgraph"), PGRAPH4, GSIZE4 },
-	{ NVxx, 0x200000, 0xf00000, N("mov"), RA, IMM },
-	{ NV5x, 0x300000, 0xff0000, N("mov"), RG, GOFF },
-	{ NVxx, 0x400000, 0xfc0000, N("jmp"), T(pred), CTARG },
-	{ NV5x, 0x440000, 0xfc0000, N("call"), T(pred), CTARG },
-	{ NV5x, 0x480000, 0xfc0000, N("ret"), T(pred) },
-	{ NVxx, 0x500000, 0xf00000, N("assert"), T(pred) },
-	{ NV5x, 0x600006, 0xffffff, N("mov"), RR, RA },
-	{ NV5x, 0x600007, 0xffffff, N("mov"), RM, RA },
-	{ NV5x, 0x60000c, 0xffffff, N("exit") },
+	{ NVxx, 0x200000, 0xf00000, N("mov"), RA, IMM },		// moves 20-bit immediate to scratch reg
+	{ NV5x, 0x300000, 0xff0000, N("mov"), RG, GOFF },		// moves 20-bit immediate to $g reg
+	{ NVxx, 0x400000, 0xfc0000, N("jmp"), T(pred), CTARG },		// jumps if condition true
+	{ NV5x, 0x440000, 0xfc0000, N("call"), T(pred), CTARG },	// calls if condition true, NVAx only
+	{ NV5x, 0x480000, 0xfc0000, N("ret"), T(pred) },		// rets if condition true, NVAx only
+	{ NV5x, 0x500000, 0xf00000, N("wait"), T(pred) },		// waits until condition true.
+	{ NV5x, 0x600006, 0xffffff, N("mov"), RR, RA },			// copies $a to $r
+	{ NV5x, 0x600007, 0xffffff, N("mov"), RM, RA },			// copies $a to $r, anding it with 0xffff8
+	{ NV5x, 0x60000c, 0xffffff, N("exit") },			// halts program execution, resets PC to 0
 	{ NV4x, 0x60000e, 0xffffff, N("exit") },
-	{ NVxx, 0x700000, 0xf00080, N("clear"), T(rpred) },
-	{ NVxx, 0x700080, 0xf00080, N("set"), T(rpred) },
+	{ NVxx, 0x700000, 0xf00080, N("clear"), T(rpred) },		// clears given flag
+	{ NVxx, 0x700080, 0xf00080, N("set"), T(rpred) },		// sets given flag
 	{ NVxx, 0, 0, OOPS },
 };
 
