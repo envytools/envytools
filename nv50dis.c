2fda5d3383fbb78bc9edae8ab3999a7ead4d7b92
Marcin Kościelnicki <koriakin@0x04.net>
2009-12-08 17:36:22 +0000

    License changes...

--- nv50dis.c
+++ nv50dis.c
@@ -1,29 +1,26 @@
-/*************************************************************************
- *                                                                       *
- *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE               *
- *                     Version 2, December 2004                          *
- *                                                                       *
- *  Copyright (C) 2004 Sam Hocevar                                       *
- *   14 rue de Plaisance, 75014 Paris, France                            *
- *  Everyone is permitted to copy and distribute verbatim or modified    *
- *  copies of this license document, and changing it is allowed as long  *
- *  as the name is changed.                                              *
- *                                                                       *
- *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE               *
- *    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION    *
- *                                                                       *
- *   0. You just DO WHAT THE FUCK YOU WANT TO.                           *
- *                                                                       *
- *************************************************************************/
-
 /*
- * Copyright (C) 2009 Marcin Kościelnicki <koriakin@0x04.net>
  *
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details.
+ * Copyright (C) 2009 Marcin Kościelnicki <koriakin@0x04.net>
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * VA LINUX SYSTEMS AND/OR ITS SUPPLIERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #include <stdio.h>
