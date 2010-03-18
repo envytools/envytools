470adca87256356bf13fa4856f2d8df022736264
Marcin Kościelnicki <koriakin@0x04.net>
2010-03-18 11:45:32 +0000

    Type fixes in headergen

--- headergen.c
+++ headergen.c
@@ -1,7 +1,8 @@
 #include "rnn.h"
 #include <stdio.h>
+#include <inttypes.h>
 
-int *strides = 0;
+uint64_t *strides = 0;
 int stridesnum = 0;
 int stridesmax = 0;
 
@@ -17,19 +18,19 @@ void printdelem (struct rnndelem *elem, uint64_t offset) {
 				if (i) printf(", ");
 				printf ("i%d", i);
 			}
-		printf (")\t(%#llx", offset + elem->offset);
+		printf (")\t(%#"PRIx64"", offset + elem->offset);
 			for (i = 0; i < stridesnum; i++)
-				printf (" + %#llx*(i%d)", strides[i], i);
+				printf (" + %#" PRIx64 "*(i%d)", strides[i], i);
 
 		printf (")\n");
 		} else
-		printf ("\t%#llx\n", offset + elem->offset);
+		printf ("\t%#"PRIx64"\n", offset + elem->offset);
 		if (elem->stride)
-		printf ("#define %s__ESIZE\t%#llx\n", elem->fullname, elem->stride);
+		printf ("#define %s__ESIZE\t%#"PRIx64"\n", elem->fullname, elem->stride);
 		if (elem->length != 1)
-		printf ("#define %s__LEN\t%#llx\n", elem->fullname, elem->length);
+		printf ("#define %s__LEN\t%#"PRIx64"\n", elem->fullname, elem->length);
 		if (elem->shr)
-		printf ("#define %s__SHR\t%#llx\n", elem->fullname, elem->shr);
+		printf ("#define %s__SHR\t%d\n", elem->fullname, elem->shr);
 	}
 	int j;
 	for (j = 0; j < elem->subelemsnum; j++) {
@@ -51,13 +52,13 @@ int main(int argc, char **argv) {
 		int j;
 		for (j = 0; j < db->enums[i]->valsnum; j++)
 			if (db->enums[i]->vals[j]->valvalid)
-				printf ("#define %s\t%#llx\n", db->enums[i]->vals[j]->name, db->enums[i]->vals[j]->value);
+				printf ("#define %s\t%#"PRIx64"\n", db->enums[i]->vals[j]->name, db->enums[i]->vals[j]->value);
 		printf ("\n");
 	}
 	for (i = 0; i < db->domainsnum; i++) {
 		printf ("/* domain %s of width %d */\n", db->domains[i]->name, db->domains[i]->width);
 		if (db->domains[i]->size) 
-			printf ("#define %s__SIZE\t%#llx\n", db->domains[i]->name, db->domains[i]->size);
+			printf ("#define %s__SIZE\t%#"PRIx64"\n", db->domains[i]->name, db->domains[i]->size);
 		int j;
 		for (j = 0; j < db->domains[i]->subelemsnum; j++) {
 			printdelem(db->domains[i]->subelems[j], 0);
