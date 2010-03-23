f2f967eeb3720c6f1ce972a6efef7bdb5c8fe15a
Luca Barbieri <luca@luca-barbieri.com>
2010-03-23 23:20:00 +0100

    expand: add tool to expand all enum and domain information
    Currently no bitfield or inline value support.
    Also no variant support.
    
    Still, this should be good enough to parse mmiotrace registers using rules.xml

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: headergen nv50_texture.h
+all: headergen expand nv50_texture.h
 
 headergen: headergen.c rnn.c rnn.h
 	gcc -o headergen headergen.c rnn.c -lxml2 -I/usr/include/libxml2 -g
+
+expand: expand.c rnn.c rnn.h
+	gcc -o expand expand.c rnn.c -lxml2 -I/usr/include/libxml2 -g
 
 nv50_texture.h: nv50_texture.xml headergen
 	./headergen nv50_texture.xml > nv50_texture.h
---
+++ expand.c
@@ -0,0 +1,47 @@
+#include "rnn.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+void expand (const char* prefix, struct rnndelem *elem, uint64_t base, int width) {
+	int i, j;
+	if(elem->length > 1024)
+		return;
+	switch (elem->type) {
+		case RNN_ETYPE_REG:
+			for(i = 0; i < elem->length; ++i) {
+					if(elem->length >= 2)
+						printf("d %s %08Lx %s(%i)", prefix, base + elem->offset + i * elem->stride, elem->fullname, i);
+					else
+						printf("d %s %08Lx %s", prefix, base + elem->offset, elem->fullname);
+
+				for(j = 0; j < elem->typesnum; ++j)
+					printf("%c%s", j ? ',' : ' ', elem->types[j]->name);
+				printf("\n");
+			}
+			break;
+		case RNN_ETYPE_STRIPE:
+		case RNN_ETYPE_ARRAY:
+			for (i = 0; i < elem->length; i++)
+				for (j = 0; j < elem->subelemsnum; j++)
+					expand(prefix, elem->subelems[j], base + elem->offset, width);
+			break;
+	}
+}
+
+int main(int argc, char **argv) {
+	rnn_init();
+	struct rnndb *db = rnn_newdb();
+	rnn_parsefile (db, argv[1]);
+	rnn_prepdb (db);
+	int i, j;
+	for(i = 0; i < db->domainsnum; ++i)
+		for (j = 0; j < db->domains[i]->subelemsnum; ++j)
+			expand (db->domains[i]->name, db->domains[i]->subelems[j], 0, db->domains[i]->width);
+	for(i = 0; i < db->enumsnum; ++i)
+		for (j = 0; j < db->enums[i]->valsnum; ++j)
+			if (db->enums[i]->vals[j]->valvalid)
+				printf("e %s %Lx %s\n", db->enums[i]->name, db->enums[i]->vals[j]->value, db->enums[i]->vals[j]->name);
+	return 0;
+}
