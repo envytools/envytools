d71c5491880962286921ac1925620e6ea0069667
Marcin Kościelnicki <koriakin@0x04.net>
2010-03-24 19:48:46 +0000

    Fix expand.

--- expand.c
+++ expand.c
@@ -12,9 +12,9 @@ void expand (const char* prefix, struct rnndelem *elem, uint64_t base, int width
 		case RNN_ETYPE_REG:
 			for(i = 0; i < elem->length; ++i) {
 					if(elem->length >= 2)
-						printf("d %s %08Lx %s(%i)", prefix, base + elem->offset + i * elem->stride, elem->fullname, i);
+						printf("d %s %08"PRIx64" %s(%i)", prefix, base + elem->offset + i * elem->stride, elem->fullname, i);
 					else
-						printf("d %s %08Lx %s", prefix, base + elem->offset, elem->fullname);
+						printf("d %s %08"PRIx64" %s", prefix, base + elem->offset, elem->fullname);
 
 				for(j = 0; j < elem->typesnum; ++j)
 					printf("%c%s", j ? ',' : ' ', elem->types[j]->name);
@@ -25,7 +25,7 @@ void expand (const char* prefix, struct rnndelem *elem, uint64_t base, int width
 		case RNN_ETYPE_ARRAY:
 			for (i = 0; i < elem->length; i++)
 				for (j = 0; j < elem->subelemsnum; j++)
-					expand(prefix, elem->subelems[j], base + elem->offset, width);
+					expand(prefix, elem->subelems[j], base + elem->offset + i * elem->stride, width);
 			break;
 	}
 }
@@ -42,6 +42,6 @@ int main(int argc, char **argv) {
 	for(i = 0; i < db->enumsnum; ++i)
 		for (j = 0; j < db->enums[i]->valsnum; ++j)
 			if (db->enums[i]->vals[j]->valvalid)
-				printf("e %s %Lx %s\n", db->enums[i]->name, db->enums[i]->vals[j]->value, db->enums[i]->vals[j]->name);
+				printf("e %s %"PRIx64" %s\n", db->enums[i]->name, db->enums[i]->vals[j]->value, db->enums[i]->vals[j]->name);
 	return 0;
 }
