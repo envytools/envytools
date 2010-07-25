f74ddcae1589c29bd7360d881385700c0e94760f
Marcin Kościelnicki <koriakin@0x04.net>
2010-07-25 12:55:37 +0000

    fuc: Uniform naming for bit-extract insns.

--- fucd-back.c
+++ fucd-back.c
@@ -396,8 +396,8 @@ static struct insn tabm[] = {
 	{ AP, 0x000000e5, 0x000000ff, N("or"), REG1, REG2, IMM16 },
 	{ AP, 0x000000e6, 0x000000ff, N("xor"), REG1, REG2, IMM16 },
 	{ AP, 0x000000e7, 0x000000ff, N("extr"), REG1, REG2, BITF16 },
-	{ AP, 0x000000ec, 0x000000ff, N("div"), REG1, REG2, IMM16 },
-	{ AP, 0x000000ed, 0x000000ff, N("mod"), REG1, REG2, IMM16 },
+	{ AP, 0x000000ec, 0x000000ff, N("div"), REG1, REG2, IMM16 }, /* NVA3+ */
+	{ AP, 0x000000ed, 0x000000ff, N("mod"), REG1, REG2, IMM16 }, /* NVA3+ */
 	{ AP, 0x000000e0, 0x000000f0, OOPS, REG1, REG2, IMM16 },
 
 	{ AP, 0x000000f0, 0x00000ffe, N("mulu"), REG2, T(i) },
@@ -485,12 +485,12 @@ static struct insn tabm[] = {
 	{ AP, 0x000000ff, 0x000f00ff, N("mulu"), T(rrr) },
 	{ AP, 0x000100ff, 0x000f00ff, N("muls"), T(rrr) },
 	{ AP, 0x000200ff, 0x000f00ff, N("sex"), T(rrr) },
-	{ AP, 0x000300f0, 0x000f00ff, N("sbext"), T(rrr) }, /* ARG1 = (ARG2 >> ARG3 & 1)?0xffffffff:0, NVA3+ */
+	{ AP, 0x000300f0, 0x000f00ff, N("extrs"), T(rrr) }, /* ARG1 = (ARG2 >> ARG3 & 1)?0xffffffff:0, NVA3+ */
 	{ AP, 0x000400ff, 0x000f00ff, N("and"), T(rrr) },
 	{ AP, 0x000500ff, 0x000f00ff, N("or"), T(rrr) },
 	{ AP, 0x000600ff, 0x000f00ff, N("xor"), T(rrr) },
-	{ AP, 0x000700f0, 0x000f00ff, N("bext"), T(rrr) }, /* ARG1 = ARG2 >> ARG3 & 1, NVA3+ */
-	{ AP, 0x000800ff, 0x000f00ff, N("xbit"), T(rrr) }, /* NV98: ARG1 = (ARG1 & 0xfffffffe) | (ARG2 >> ARG3 & 1) ; NVA3+: same as bext */
+	{ AP, 0x000700f0, 0x000f00ff, N("extr"), T(rrr) }, /* ARG1 = ARG2 >> ARG3 & 1, NVA3+ */
+	{ AP, 0x000800ff, 0x000f00ff, N("xbit"), T(rrr) }, /* NV98: ARG1 = (ARG1 & 0xfffffffe) | (ARG2 >> ARG3 & 1) ; NVA3+: ARG1 = ARG2 >> ARG3 & 1 */
 	{ AP, 0x000c00ff, 0x000f00ff, N("div"), T(rrr) },
 	{ AP, 0x000d00ff, 0x000f00ff, N("mod"), T(rrr) },
 	{ AP, 0x000f00ff, 0x000f00ff, N("iord"), REG3, IORR },
