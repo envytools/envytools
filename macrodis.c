3df9f63a9409b093b7682819d05936c8fc02d758
Marcin Kościelnicki <koriakin@0x04.net>
2010-10-24 17:50:54 +0000

    macrodis: Nailed down the extr insn fully.

--- macrodis.c
+++ macrodis.c
@@ -30,11 +30,13 @@
  */
 
 static struct bitfield mthdoff = { { 14, 12 }, BF_UNSIGNED, 2 };
-static struct bitfield bfposoff = { 17, 5 };
+static struct bitfield bfsrcposoff = { 17, 5 };
 static struct bitfield bfszoff = { 22, 5 };
+static struct bitfield bfdstposoff = { 27, 5 };
 static struct bitfield mimmoff = { { 14, 18 }, BF_SIGNED };
 #define MTHD atomimm, &mthdoff
-#define BFPOS atomimm, &bfposoff
+#define BFSRCPOS atomimm, &bfsrcposoff
+#define BFDSTPOS atomimm, &bfdstposoff
 #define BFSZ atomimm, &bfszoff
 #define MIMM atomimm, &mimmoff
 
@@ -56,7 +58,7 @@ static struct insn tabsdst[] = {
 
 static struct insn tabm[] = {
 	{ AP, 0x00000001, 0x0000000f, N("add"), REG1, REG2, MIMM },
-	{ AP, 0x00000002, 0x0000000f, N("extr"), REG1, REG3, BFPOS, BFSZ },
+	{ AP, 0x00000002, 0x0000000f, N("extr"), REG1, REG2, REG3, BFSRCPOS, BFSZ, BFDSTPOS }, // take REG2, replace BFSZ bits starting at BFDSTPOS with BFSZ bits starting at BFSRCPOS in REG3.
 	{ AP, 0x00000005, 0x0000000f, N("read"), REG1, MTHD },
 	{ AP, 0, 0, OOPS },
 };
