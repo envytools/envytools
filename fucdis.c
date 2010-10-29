26a035ab5cead610767b567e296ea30f8ae16ee0
Marcin Kościelnicki <koriakin@0x04.net>
2010-10-29 08:47:58 +0000

    fucdis: Add movw pseudo-insn: mov with "wrap ok" 16-bit argument.

--- fucdis.c
+++ fucdis.c
@@ -104,6 +104,7 @@ static struct bitfield cimm2off = { 20, 6 };
 #define IMM8 atomimm, &imm8off
 #define IMM16S atomimm, &imm16soff
 #define IMM8S atomimm, &imm8soff
+#define IMM16W atomimm, &imm16woff
 #define IMM16H atomimm, &imm16hoff
 #define IMM8H atomimm, &imm8hoff
 #define STRAP atomimm, &strapoff
@@ -409,6 +410,7 @@ static struct insn tabm[] = {
 	{ AP, 0x000005f0, 0x00000ffe, T(ol0), N("or"), REG2, T(i) },
 	{ AP, 0x000006f0, 0x00000ffe, T(ol0), N("xor"), REG2, T(i) },
 	{ AP, 0x000007f0, 0x00000ffe, T(ol0), N("mov"), REG2, T(is) },
+	{ AP, 0x000007f0, 0x00000fff, T(ol0), N("movw"), REG2, IMM16W },
 	{ AP, 0x000009f0, 0x00000ffe, T(ol0), N("bset"), REG2, T(i) },
 	{ AP, 0x00000af0, 0x00000ffe, T(ol0), N("bclr"), REG2, T(i) },
 	{ AP, 0x00000bf0, 0x00000ffe, T(ol0), N("btgl"), REG2, T(i) },
