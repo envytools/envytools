1c771ed7578ccc73db131560ad87f2422abb4cd6
Marcin Kościelnicki <koriakin@0x04.net>
2011-04-21 16:07:45 +0000

    nvc0/isa: Add some quadop modifiers.

--- nvc0dis.c
+++ nvc0dis.c
@@ -803,7 +803,7 @@ static struct insn tabm[] = {
 	{ 0x3000000000000000ull, 0xf800000000000007ull, N("add"), T(fmf), T(ias), T(farm), N("f32"), DST, T(acout), T(neg1), N("mul"), T(fmz), SRC1, T(fs2w3), T(neg2), T(is3) },
 	{ 0x3800000000000000ull, 0xf800000000000007ull, N("slct"), T(faf), N("b32"), DST, SRC1, T(fs2w3), T(setit), N("f32"), T(is3) },
 	// 40?
-	{ 0x4800000000000000ull, 0xf800000000000007ull, N("quadop"), N("f32"), T(qop0), T(qop1), T(qop2), T(qop3), DST, T(qs1), SRC1, T(fs2) },
+	{ 0x4800000000000000ull, 0xf800000000000007ull, N("quadop"), T(faf), T(farm), N("f32"), T(qop0), T(qop1), T(qop2), T(qop3), DST, T(acout), T(qs1), SRC1, SRC2 },
 	{ 0x5000000000000000ull, 0xf800000000000007ull, N("add"), T(faf), T(fas), T(farm), N("f32"), DST, T(acout), T(neg1), T(abs1), SRC1, T(neg2), T(abs2), T(fs2) },
 	{ 0x5800000000000000ull, 0xf800000000000007ull, N("mul"), T(mulf), T(fmz), T(fmf), T(ias), T(farm), T(fmneg), N("f32"), DST, T(acout), SRC1, T(fs2) },
 	{ 0x6000000000000000ull, 0xf800000000000027ull, N("presin"), N("f32"), DST, T(neg2), T(abs2), T(fs2) },
