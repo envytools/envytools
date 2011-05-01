0df90601d2b558ef7457d83a349da33f8b85d87a
Martin Peres <martin.peres@ensi-bourges.fr>
2011-05-01 21:47:32 +0200

    nvagetbios: Always print the vbios even if the checksum fails + various fixes

--- nvagetbios.c
+++ nvagetbios.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define NV_PRAMIN_OFFSET            0x00700000
 #define NV_PROM_SIZE                0x00010000
@@ -25,14 +26,14 @@ static int nv_cksum(const uint8_t *data, unsigned int length)
 	return 1;
 }
 
-int vbios_extract_pramin(int cnum)
+/* vbios should at least be NV_PROM_SIZE bytes long */
+int vbios_extract_pramin(int cnum, uint8_t *vbios, int *length)
 {
-	uint8_t vbios[NV_PROM_SIZE];
 	uint32_t old_bar0_pramin = 0;
 	uint32_t ret = 0;
 	int i;
 
-	fprintf(stderr, "Attempts to extract the vbios from the card %i (nv%02x) using PRAMIN\n",
+	fprintf(stderr, "Attempt to extract the vbios from card %i (nv%02x) using PRAMIN\n",
 			cnum, nva_cards[cnum].chipset);
 
 	if (nva_cards[cnum].chipset >= 0x50) {
@@ -54,8 +55,12 @@ int vbios_extract_pramin(int cnum)
 		vbios[i] = nva_rd8(cnum, NV_PRAMIN_OFFSET + i);
 
 	ret = nv_cksum(vbios, vbios[2] * 512);
-	if (ret)
-		fwrite(vbios, 1, vbios[2] * 512, stdout);
+
+	if (length)
+		*length = vbios[2] * 512;
+
+	/* print the vbios on stdout */
+	fwrite(vbios, 1, vbios[2] * 512, stdout);
 
 out:
 	if (nva_cards[cnum].chipset >= 0x50)
@@ -66,17 +71,20 @@ out:
 
 void usage(int error_code)
 {
-	fprintf(stderr, "\nUsage: nvagetbios [-c card_number] -s extraction_method > my_vbios.rom\n");
+	fprintf(stderr, "\nUsage: nvagetbios [-c card_number] [-s extraction_method] > my_vbios.rom\n");
 	exit(error_code);
 }
 
 int main(int argc, char **argv) {
-	assert(!nva_init());
+	uint8_t vbios[NV_PROM_SIZE];
 	int c;
 	int cnum =0;
-	char source[21];
+	char const *source = NULL;
 	int success = 0;
 
+	assert(!nva_init());
+
+	/* Arguments parsing */
 	while ((c = getopt (argc, argv, "hc:s:")) != -1)
 		switch (c) {
 			case 'h':
@@ -86,7 +94,7 @@ int main(int argc, char **argv) {
 				sscanf(optarg, "%d", &cnum);
 				break;
 			case 's':
-				strncpy(source, optarg, 20);
+				source = optarg;
 				break;
 		}
 	if (cnum >= nva_cardsnum) {
@@ -97,8 +105,14 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
+	if (source == NULL) {
+		fprintf(stderr, "No extraction method specified (using -s extraction_method). Defaulting to PRAMIN.\n");
+		source = "pramin";
+	}
+
+	/* Extraction */
 	if (strcasecmp(source, "pramin") == 0)
-		success = vbios_extract_pramin(cnum);
+		success = vbios_extract_pramin(cnum, vbios, NULL);
 	else {
 		fprintf(stderr, "Unknown vbios extraction method.\n");
 		usage(1);
@@ -106,9 +120,8 @@ int main(int argc, char **argv) {
 
 	if (success) {
 		fprintf(stderr, "Extraction done. Valid checksum.\n");
-		return 2;
 	} else {
-		fprintf(stderr, "Invalid checksum. Try another retrieval method.\n");
+		fprintf(stderr, "Invalid checksum. You may want to try another retrieval method.\n");
 		return 2;
 	}
 
