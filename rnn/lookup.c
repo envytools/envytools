e9b187152d677f8695108069dc9f5d081785e3ad
Martin Peres <martin.peres@ensi-bourges.fr>
2011-07-14 14:19:43 +0200

    lookup: ease the interface

--- rnn/lookup.c
+++ rnn/lookup.c
@@ -4,56 +4,127 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <string.h>
+#include <getopt.h>
+
+void usage()
+{
+	printf ("Usage:\n"
+			"\tlookup [-f file.xml] [-a NVXX] -e enum-name [-- -v attribute value] value\n"
+			"\tlookup [-f file.xml] [-a NVXX] -b bitset-name [-- -v attribute value] value\n"
+			"\tlookup [-f file.xml] [-a NVXX] [-d domain-name] [-- -v attribute value] address\n"
+			"\tlookup [-f file.xml] [-a NVXX] [-d domain-name] [-- -v attribute value] address value\n"
+		);
+	exit(2);
+}
 
 int main(int argc, char **argv) {
+	char *file = "nv_mmio.xml";
+	char *name = "NV_MMIO";
+	char *variant = NULL;
+	char c, mode = 'd';
+	uint64_t reg, val = 0;
+	struct rnndeccontext *vc;
+
 	rnn_init();
-	if (argc < 4) {
-		printf ("Usage:\n"
-				"\tlookup file.xml enum-name value\n"
-				"\tlookup file.xml bitset-name value\n"
-				"\tlookup file.xml domain-name address\n"
-				"\tlookup file.xml domain-name address value\n"
-			);
-		return 0;
+	if (argc < 2) {
+		usage();
 	}
 	struct rnndb *db = rnn_newdb();
-	rnn_parsefile (db, argv[1]);
-	rnn_prepdb (db);
-	struct rnndeccontext *vc = rnndec_newcontext(db);
-	vc->colors = &rnndec_colorsterm;
-	while (!strcmp (argv[2], "-v")) {
-		rnndec_varadd(vc, argv[3], argv[4]);
-		argv += 3;
-		argc -= 3;
-	}
-	struct rnndomain *dom = rnn_finddomain (db, argv[2]);
-	struct rnnbitset *bs = rnn_findbitset (db, argv[2]);
-	struct rnnenum *en = rnn_findenum (db, argv[2]);
-	uint64_t a, b;
-	a = strtoull(argv[3], 0, 0);
-	if (argc > 4)
-		b = strtoull(argv[4], 0, 0);
-	if (dom) {
-		struct rnndecaddrinfo *info = rnndec_decodeaddr(vc, dom, a, 0);
-		if (info)
-			printf ("%s\n", info->name);
-		if (info && info->typeinfo)
-			printf ("%s\n", rnndec_decodeval(vc, info->typeinfo, b, info->width));
-	} else if (bs) {
-	} else if (en) {
-		int i;
-		int dec = 0;
-		for (i = 0; i < en->valsnum; i++)
-			if (en->vals[i]->valvalid && en->vals[i]->value == a) {
-				printf ("%s\n", en->vals[i]->name);
-				dec = 1;
+
+	/* Arguments parsing */
+	while ((c = getopt (argc, argv, "f:a:d:e:b:")) != -1) {
+		switch (c) {
+			case 'f':
+				file = strdup(optarg);
 				break;
-			}
-		if (!dec)
-			printf ("%#"PRIx64"\n", a);
-	} else {
-		printf ("Not an enum, bitset, or domain: %s\n", argv[2]);
-		return 0;
+			case 'e':
+				mode = 'e';
+				name = strdup(optarg);
+				break;
+			case 'b':
+				mode = 'b';
+				name = strdup(optarg);
+				break;
+			case 'd':
+				mode = 'd';
+				name = strdup(optarg);
+				break;
+			case  'a':
+				variant = strdup(optarg);
+				break;
+		}
 	}
-	return 0;
+
+	rnn_parsefile (db, file);
+	rnn_prepdb (db);
+	vc = rnndec_newcontext(db);
+	vc->colors = &rnndec_colorsterm;
+
+	if (variant)
+		rnndec_varadd(vc, "chipset", variant);
+
+	/* Parse extra arguments */
+	while (!strcmp (argv[optind], "-v")) {
+		rnndec_varadd(vc, argv[optind+1], argv[optind+2]);
+		optind+=3;
+	}
+
+	if (optind >= argc) {
+		fprintf (stderr, "No address specified.\n");
+		return 1;
+	}
+
+	reg = strtoull(argv[optind], 0, 16);
+	if (optind + 1 < argc)
+		val = strtoull(argv[optind + 1], 0, 16);
+
+	if (mode == 'e') {
+		struct rnnenum *en = rnn_findenum (db, name);
+		if (en) {
+			int i;
+			int dec = 0;
+			for (i = 0; i < en->valsnum; i++)
+				if (en->vals[i]->valvalid && en->vals[i]->value == reg) {
+					printf ("%s\n", en->vals[i]->name);
+					dec = 1;
+					break;
+				}
+			if (!dec)
+				printf ("%#"PRIx64"\n", reg);
+
+			return 0;
+		} else {
+			fprintf(stderr, "Not an enum: '%s'\n", name);
+			return 1;
+		}
+	} else if (mode == 'b') {
+		struct rnnbitset *bs = rnn_findbitset (db, name);
+
+		if (bs) {
+			printf("TODO\n");
+			return 0;
+		} else {
+			fprintf(stderr, "Not a bitset: '%s'\n", name);
+			return 1;
+		}
+
+	} else if (mode == 'd') {
+		struct rnndomain *dom = rnn_finddomain (db, name);
+
+		if (dom) {
+			struct rnndecaddrinfo *info = rnndec_decodeaddr(vc, dom, reg, 0);
+			if (info && info->typeinfo)
+				printf ("%s => %s\n", info->name, rnndec_decodeval(vc, info->typeinfo, val, info->width));
+			else if (info)
+				printf ("%s\n", info->name);
+			else
+				return 1;
+			return 0;
+		} else {
+			fprintf(stderr, "Not a domain: '%s'\n", name);
+			return 1;
+		}
+	} else {
+		return 1;
+	}
 }
