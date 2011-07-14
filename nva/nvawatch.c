71a4016739e27705be836eeb81cffba4162036a3
Marcin Kościelnicki <koriakin@0x04.net>
2011-07-14 12:55:48 +0200

    nva: Add "watch a register" program.

---
+++ nva/nvawatch.c
@@ -0,0 +1,40 @@
+#include "nva.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+	if (nva_init()) {
+		fprintf (stderr, "PCI init failure!\n");
+		return 1;
+	}
+	int c;
+	int cnum =0;
+	while ((c = getopt (argc, argv, "c:")) != -1)
+		switch (c) {
+			case 'c':
+				sscanf(optarg, "%d", &cnum);
+				break;
+		}
+	if (cnum >= nva_cardsnum) {
+		if (nva_cardsnum)
+			fprintf (stderr, "No such card.\n");
+		else
+			fprintf (stderr, "No cards found.\n");
+		return 1;
+	}
+	int32_t a, b = 4, i;
+	if (optind >= argc) {
+		fprintf (stderr, "No address specified.\n");
+		return 1;
+	}
+	sscanf (argv[optind], "%x", &a);
+	uint32_t val = nva_rd32(cnum, a);
+	printf ("%08x\n", val);
+	while (1) {
+		uint32_t nval = nva_rd32(cnum, a);
+		if (nval != val)
+			printf ("%08x\n", nval);
+		val = nval;
+	}
+	return 0;
+}
