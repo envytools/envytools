92a2c251bb6696057d15680280fcfe6d4ea2a4ed
Marcin Kościelnicki <koriakin@0x04.net>
2011-12-08 14:59:43 +0100

    nva: Add VRAM bulk upload/download programs.

---
+++ nva/nvadownload.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2010-2011 Marcin Kościelnicki <koriakin@0x04.net>
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * VA LINUX SYSTEMS AND/OR ITS SUPPLIERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "nva.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+	if (nva_init()) {
+		fprintf (stderr, "PCI init failure!\n");
+		return 1;
+	}
+	int c;
+	int cnum =0;
+	while ((c = getopt (argc, argv, "c:")) != -1)
+		switch (c) {
+			case 'c':
+				sscanf(optarg, "%d", &cnum);
+				break;
+		}
+	if (cnum >= nva_cardsnum) {
+		if (nva_cardsnum)
+			fprintf (stderr, "No such card.\n");
+		else
+			fprintf (stderr, "No cards found.\n");
+		return 1;
+	}
+	uint32_t a, b;
+	if (optind >= argc) {
+		fprintf (stderr, "No address specified.\n");
+		return 1;
+	}
+	if (optind + 1 >= argc) {
+		fprintf (stderr, "No length specified.\n");
+		return 1;
+	}
+	FILE *file = stdout;
+	if (optind + 2 < argc) {
+		file = fopen(argv[optind+2], "wb");
+		if (!file) {
+			perror("fopen");
+			return 1;
+		}
+	}
+	sscanf (argv[optind], "%x", &a);
+	sscanf (argv[optind+1], "%x", &b);
+	b += a;
+	while (a != b) {
+		nva_wr32(cnum, 0x1700, a >> 16);
+		int c = nva_rd8(cnum, 0x700000 | (a&0xffff));
+		putc(c, file);
+		a++;
+	}
+	return 0;
+}
