bee3e3031b415ad8cf10044ffb852b824f43f923
Marcin Kościelnicki <koriakin@0x04.net>
2012-02-05 01:23:00 +0100

    nva: Add a "reg value statistics" tool.

---
+++ nva/nvapeekstat.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2010-2011 Marcin Kościelnicki <koriakin@0x04.net>
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * VA LINUX SYSTEMS AND/OR ITS SUPPLIERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "nva.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int cmp(const void *pa, const void *pb) {
+	const uint32_t *a = pa;
+	const uint32_t *b = pb;
+	return (*a > *b) - (*a < *b);
+}
+
+int main(int argc, char **argv) {
+	if (nva_init()) {
+		fprintf (stderr, "PCI init failure!\n");
+		return 1;
+	}
+	int c;
+	int cnum =0;
+	while ((c = getopt (argc, argv, "c:")) != -1)
+		switch (c) {
+			case 'c':
+				sscanf(optarg, "%d", &cnum);
+				break;
+		}
+	if (cnum >= nva_cardsnum) {
+		if (nva_cardsnum)
+			fprintf (stderr, "No such card.\n");
+		else
+			fprintf (stderr, "No cards found.\n");
+		return 1;
+	}
+	int32_t a, b = 10000, i;
+	if (optind >= argc) {
+		fprintf (stderr, "No address specified.\n");
+		return 1;
+	}
+	sscanf (argv[optind], "%x", &a);
+	if (optind + 1 < argc)
+		sscanf (argv[optind + 1], "%d", &b);
+	uint32_t tab[b];
+	for (i = 0; i < b; i++)
+		tab[i] = nva_rd32(cnum, a);
+	qsort(tab, b, sizeof *tab, cmp);
+	uint32_t prev = tab[0];
+	int cnt = 0;
+	for (i = 0; i < b; i++) {
+		if (tab[i] != prev) {
+			printf("%08x: %d\n", prev, cnt);
+			cnt = 0;
+			prev = tab[i];
+		}
+		cnt++;
+	}
+	printf("%08x: %d\n", prev, cnt);
+	return 0;
+}
