18a12119ebfa800ae523aa5355b08b2fae86b57f
Marcin Kościelnicki <koriakin@0x04.net>
2014-01-11 20:35:35 +0000

    nva: Also look for nvidia devices with 048000 class.

--- nva/nva.c
+++ nva/nva.c
@@ -33,71 +33,43 @@ struct nva_card *nva_cards = 0;
 int nva_cardsnum = 0;
 int nva_cardsmax = 0;
 
+struct pci_id_match nv_match[4] = {
+	{0x104a, 0x0009, PCI_MATCH_ANY, PCI_MATCH_ANY, 0, 0},
+	{0x12d2, PCI_MATCH_ANY, PCI_MATCH_ANY, PCI_MATCH_ANY, 0x30000, 0xffff0000},
+	{0x10de, PCI_MATCH_ANY, PCI_MATCH_ANY, PCI_MATCH_ANY, 0x30000, 0xffff0000},
+	{0x10de, PCI_MATCH_ANY, PCI_MATCH_ANY, PCI_MATCH_ANY, 0x48000, 0xffffff00},
+};
+
 int nva_init() {
 	int ret;
 	ret = pci_system_init();
 	if (ret)
 		return -1;
-	struct pci_id_match nv_match = {0x10de, PCI_MATCH_ANY, PCI_MATCH_ANY, PCI_MATCH_ANY, 0x30000, 0xffff0000};
-	struct pci_device_iterator* it = pci_id_match_iterator_create(&nv_match);
-	if (!it) {
-		pci_system_cleanup();
-		return -1;
-	}
-
-	struct pci_device *dev;
-	while ((dev = pci_device_next(it))) {
-		struct nva_card c = { 0 };
-		ret = pci_device_probe(dev);
-		if (ret) {
-			fprintf (stderr, "WARN: Can't probe %04x:%02x:%02x.%x\n", dev->domain, dev->bus, dev->dev, dev->func);
-			continue;
-		}
-		c.pci = dev;
-		ADDARRAY(nva_cards, c);
-	}
-	pci_iterator_destroy(it);
-
-	struct pci_id_match nv_sgs_match = {0x12d2, PCI_MATCH_ANY, PCI_MATCH_ANY, PCI_MATCH_ANY, 0x30000, 0xffff0000};
-	it = pci_id_match_iterator_create(&nv_sgs_match);
-	if (!it) {
-		pci_system_cleanup();
-		return -1;
-	}
-
-	while ((dev = pci_device_next(it))) {
-		struct nva_card c = { 0 };
-		ret = pci_device_probe(dev);
-		if (ret) {
-			fprintf (stderr, "WARN: Can't probe %04x:%02x:%02x.%x\n", dev->domain, dev->bus, dev->dev, dev->func);
-			continue;
-		}
-		c.pci = dev;
-		ADDARRAY(nva_cards, c);
-	}
-	pci_iterator_destroy(it);
-
-	struct pci_id_match nv01_match = {0x104a, 0x0009, PCI_MATCH_ANY, PCI_MATCH_ANY, 0, 0};
-	it = pci_id_match_iterator_create(&nv01_match);
-	if (!it) {
-		pci_system_cleanup();
-		return -1;
-	}
-
-	while ((dev = pci_device_next(it))) {
-		struct nva_card c = { 0 };
-		ret = pci_device_probe(dev);
-		if (ret) {
-			fprintf (stderr, "WARN: Can't probe %04x:%02x:%02x.%x\n", dev->domain, dev->bus, dev->dev, dev->func);
-			continue;
-		}
-		c.pci = dev;
-		ADDARRAY(nva_cards, c);
-	}
-	pci_iterator_destroy(it);
-
 	int i;
+
+	for (i = 0; i < ARRAY_SIZE(nv_match); i++) {
+		struct pci_device_iterator* it = pci_id_match_iterator_create(&nv_match[i]);
+		if (!it) {
+			pci_system_cleanup();
+			return -1;
+		}
+
+		struct pci_device *dev;
+		while ((dev = pci_device_next(it))) {
+			struct nva_card c = { 0 };
+			ret = pci_device_probe(dev);
+			if (ret) {
+				fprintf (stderr, "WARN: Can't probe %04x:%02x:%02x.%x\n", dev->domain, dev->bus, dev->dev, dev->func);
+				continue;
+			}
+			c.pci = dev;
+			ADDARRAY(nva_cards, c);
+		}
+		pci_iterator_destroy(it);
+	}
+
 	for (i = 0; i < nva_cardsnum; i++) {
+		struct pci_device *dev;
 		dev = nva_cards[i].pci;
 		ret = pci_device_map_range(dev, dev->regions[0].base_addr, dev->regions[0].size, PCI_DEV_MAP_FLAG_WRITABLE, &nva_cards[i].bar0);
 		if (ret) {
