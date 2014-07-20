bab7dd4e48d25722fc5c99a06a4950a3ad44eb19
Marcin Kościelnicki <koriakin@0x04.net>
2014-07-20 05:55:09 +0200

    hwtest: VP1 ops 0x00-0x5f.

--- hwtest/vp1.c
+++ hwtest/vp1.c
@@ -105,14 +105,299 @@ static uint32_t vp1_shr(uint32_t a, int32_t b, int u) {
 	}
 }
 
+static uint32_t vp1_abs(uint32_t x) {
+	if (x & 0x80000000)
+		return -x;
+	else
+		return x;
+}
+
+static uint8_t vp1_shrb(int32_t a, uint8_t b) {
+	int shr = b & 0xf;
+	if (shr & 8)
+		shr |= -8;
+	if (shr < 0) {
+		return a << -shr;
+	} else {
+		return a >> shr;
+	}
+}
+
 static void simulate_op(struct vp1_ctx *ctx, uint32_t opcode) {
-	uint32_t op = opcode >> 24;
+	uint32_t op = opcode >> 24 & 0x7f;
 	uint32_t src1 = opcode >> 14 & 0x1f;
+	uint32_t src2 = opcode >> 9 & 0x1f;
 	uint32_t dst = opcode >> 19 & 0x1f;
 	uint32_t imm = extrs(opcode, 3, 11);
 	uint32_t cdst = opcode & 7;
-	uint32_t cr, res, s1;
+	uint32_t cr, res, s1, s2, s2s;
+	int flag = opcode >> 5 & 0xf;
+	uint32_t cond;
+	int src2s;
+	int32_t sub, ss1, ss2;
+	int i;
+	int u;
+	cond = ctx->c[opcode >> 3 & 3];
+	if (flag == 4) {
+		src2s = (src2 & 0x1c) | ((src2 + (cond >> flag)) & 3);
+	} else {
+		src2s = src2 ^ ((cond >> flag) & 1);
+	}
 	switch (op) {
+		case 0x08:
+		case 0x09:
+		case 0x0a:
+		case 0x0b:
+		case 0x0c:
+		case 0x0d:
+		case 0x0e:
+		case 0x18:
+		case 0x19:
+		case 0x1a:
+		case 0x1b:
+		case 0x1c:
+		case 0x1d:
+		case 0x1e:
+		case 0x28:
+		case 0x29:
+		case 0x2a:
+		case 0x2b:
+		case 0x2c:
+		case 0x2d:
+		case 0x2e:
+		case 0x38:
+		case 0x39:
+		case 0x3a:
+		case 0x3b:
+		case 0x3c:
+		case 0x3d:
+		case 0x3e:
+			u = !!(op & 0x10);
+			s1 = read_r(ctx, src1);
+			s2 = read_r(ctx, src2s);
+			res = 0;
+			for (i = 0; i < 4; i++) {
+				ss1 = s1 >> i * 8;
+				if (op & 0x20)
+					ss2 = opcode >> 3;
+				else
+					ss2 = s2 >> i * 8;
+				ss1 = (int8_t)ss1;
+				ss2 = (int8_t)ss2;
+				if (u) {
+					ss1 &= 0xff;
+					ss2 &= 0xff;
+				}
+				switch (op & 0xf) {
+					case 0x8:
+						sub = vp1_min(ss1, ss2);
+						break;
+					case 0x9:
+						sub = vp1_max(ss1, ss2);
+						break;
+					case 0xa:
+						sub = vp1_abs(ss1);
+						break;
+					case 0xb:
+						sub = -ss1;
+						break;
+					case 0xc:
+						sub = ss1 + ss2;
+						break;
+					case 0xd:
+						sub = ss1 - ss2;
+						break;
+					case 0xe:
+						sub = vp1_shrb(ss1, ss2);
+						break;
+				}
+				if ((op & 0xf) != 0xe) {
+					if (u) {
+						if (sub < 0)
+							sub = 0;
+						if (sub > 0xff)
+							sub = 0xff;
+					} else {
+						if (sub < -0x80)
+							sub = -0x80;
+						if (sub > 0x7f)
+							sub = 0x7f;
+					}
+				}
+				sub &= 0xff;
+				res |= sub << i * 8;
+			}
+			write_r(ctx, dst, res);
+			write_c_s(ctx, cdst, 0);
+			break;
+		case 0x01:
+		case 0x02:
+		case 0x11:
+		case 0x12:
+		case 0x21:
+		case 0x22:
+		case 0x31:
+		case 0x32:
+			u = !!(op & 0x10);
+			s1 = read_r(ctx, src1);
+			s2 = read_r(ctx, src2);
+			res = 0;
+			for (i = 0; i < 4; i++) {
+				if (opcode & 4)
+					ss1 = (int8_t)(s1 >> i * 8) << 1;
+				else
+					ss1 = s1 >> i * 8 & 0xff;
+				if (op & 0x20) {
+					if (op & 2) {
+						ss2 = (int8_t)opcode;
+					} else {
+						imm = (opcode & 1) << 5 | src2;
+						ss2 = (int8_t)(imm << 2);
+					}
+				} else {
+					ss2 = (int8_t)(s2 >> i * 8);
+				}
+				if (opcode & 2)
+					ss2 <<= 1;
+				else
+					ss2 &= 0xff;
+				sub = ss1 * ss2;
+				if (u) {
+					if (sub & 0x80000000)
+						sub = 0;
+					if (opcode & 0x100)
+						sub += 0x80;
+					sub >>= 8;
+					if (sub >= 0x100)
+						sub = 0xff;
+				} else {
+					if (opcode & 0x100)
+						sub += 0x100;
+					sub >>= 9;
+					if (sub == 0x80)
+						sub = 0x7f;
+				}
+				sub &= 0xff;
+				res |= sub << i * 8;
+			}
+			write_r(ctx, dst, res);
+			break;
+		case 0x25:
+		case 0x26:
+		case 0x27:
+			s1 = read_r(ctx, src1);
+			imm = opcode >> 3 & 0xff;
+			imm *= 0x01010101;
+			if (op == 0x25) {
+				res = s1 & imm;
+			} else if (op == 0x26) {
+				res = s1 | imm;
+			} else if (op == 0x27) {
+				res = s1 ^ imm;
+			}
+			write_r(ctx, dst, res);
+			write_c_s(ctx, cdst, 0);
+			break;
+		case 0x41:
+		case 0x42:
+		case 0x48:
+		case 0x49:
+		case 0x4a:
+		case 0x4b:
+		case 0x4c:
+		case 0x4d:
+		case 0x4e:
+		case 0x51:
+		case 0x58:
+		case 0x59:
+		case 0x5a:
+		case 0x5b:
+		case 0x5c:
+		case 0x5d:
+		case 0x5e:
+			s1 = read_r(ctx, src1);
+			s2 = read_r(ctx, src2);
+			s2s = read_r(ctx, src2s);
+			if (op == 0x41 || op == 0x51) {
+				res = sext(s1, 15) * sext(s2s, 15);
+				cr = cond_s_f(res, s1);
+			} else if (op == 0x42) {
+				switch (opcode >> 3 & 0xf) {
+					case 0x0:
+						res = 0;
+						break;
+					case 0x1:
+						res = ~s1 & ~s2;
+						break;
+					case 0x2:
+						res = ~s1 & s2;
+						break;
+					case 0x3:
+						res = ~s1;
+						break;
+					case 0x4:
+						res = s1 & ~s2;
+						break;
+					case 0x5:
+						res = ~s2;
+						break;
+					case 0x6:
+						res = s1 ^ s2;
+						break;
+					case 0x7:
+						res = ~s1 | ~s2;
+						break;
+					case 0x8:
+						res = s1 & s2;
+						break;
+					case 0x9:
+						res = ~s1 ^ s2;
+						break;
+					case 0xa:
+						res = s2;
+						break;
+					case 0xb:
+						res = ~s1 | s2;
+						break;
+					case 0xc:
+						res = s1;
+						break;
+					case 0xd:
+						res = s1 | ~s2;
+						break;
+					case 0xe:
+						res = s1 | s2;
+						break;
+					case 0xf:
+						res = 0xffffffff;
+						break;
+				}
+				cr = cond_s(res);
+			} else if (op == 0x48 || op == 0x58) {
+				res = vp1_min(s1, s2s);
+				cr = cond_s_f(res, s1);
+			} else if (op == 0x49 || op == 0x59) {
+				res = vp1_max(s1, s2s);
+				cr = cond_s_f(res, s1);
+			} else if (op == 0x4a || op == 0x5a) {
+				res = vp1_abs(s1);
+				cr = cond_s_f(res, s1);
+			} else if (op == 0x4b || op == 0x5b) {
+				res = -s1;
+				cr = cond_s_f(res, 0);
+			} else if (op == 0x4c || op == 0x5c) {
+				res = s1 + s2s;
+				cr = cond_s_f(res, s1);
+			} else if (op == 0x4d || op == 0x5d) {
+				res = s1 - s2s;
+				cr = cond_s_f(res, s1);
+			} else if (op == 0x4e || op == 0x5e) {
+				res = vp1_shr(s1, s2s, op == 0x5e);
+				cr = cond_s_f(res, s1);
+			}
+			write_r(ctx, dst, res);
+			write_c_s(ctx, cdst, cr);
+			break;
 		case 0x61:
 		case 0x62:
 		case 0x63:
@@ -159,10 +444,7 @@ static void simulate_op(struct vp1_ctx *ctx, uint32_t opcode) {
 				res = vp1_shr(s1, imm, op == 0x7e);
 				cr = cond_s_f(res, s1);
 			} else if (op == 0x7a) {
-				if (s1 & 0x80000000)
-					res = -s1;
-				else
-					res = s1;
+				res = vp1_abs(s1);
 				cr = cond_s_f(res, s1);
 			} else if (op == 0x7b) {
 				res = -s1;
@@ -177,23 +459,59 @@ static void simulate_op(struct vp1_ctx *ctx, uint32_t opcode) {
 		case 0x75:
 			write_r(ctx, dst, (read_r(ctx, dst) & 0xffff) | (opcode & 0xffff) << 16);
 			break;
+		case 0x45:
+			write_r(ctx, src1, ((int32_t)read_r(ctx, src1) >> 4));
+			break;
+		case 0x1f:
+		case 0x2f:
+		case 0x3f:
+		case 0x40:
+		case 0x43:
+		case 0x44:
+		case 0x46:
+		case 0x47:
+		case 0x50:
+		case 0x52:
+		case 0x53:
+		case 0x54:
+		case 0x55:
+		case 0x56:
+		case 0x57:
+		case 0x5f:
+		case 0x60:
+		case 0x66:
+		case 0x67:
+		case 0x6f:
+		case 0x70:
+		case 0x72:
+		case 0x73:
+		case 0x74:
+		case 0x76:
+		case 0x77:
+		case 0x7f:
+			write_c_s(ctx, cdst, 0);
+			break;
 	}
 }
 
 static int test_isa_s(struct hwtest_ctx *ctx) {
 	int i, j;
+	nva_wr32(ctx->cnum, 0x200, 0xfffffffd);
+	nva_wr32(ctx->cnum, 0x200, 0xffffffff);
 	for (i = 0; i < 1000000; i++) {
-		nva_wr32(ctx->cnum, 0x200, 0xfffffffd);
-		nva_wr32(ctx->cnum, 0x200, 0xffffffff);
 		uint32_t opcode = (uint32_t)jrand48(ctx->rand48);
-		opcode &= 0x1fffffff;
-		opcode |= 0x60000000;
-		uint32_t op = opcode >> 24;
+		uint32_t op = opcode >> 24 & 0x7f;
 		if (op == 0x6a || op == 0x6b)
 			continue;
 		struct vp1_ctx octx, ectx, nctx;
-		for (j = 0; j < 31; j++)
-			octx.r[j] = jrand48(ctx->rand48);
+		for (j = 0; j < 31; j++) {
+			uint32_t val = jrand48(ctx->rand48);
+			int which = jrand48(ctx->rand48) & 0xf;
+			if (which < 4) {
+				val &= ~(0x7f << which * 8);
+			}
+			octx.r[j] = val;
+		}
 		for (j = 0; j < 4; j++)
 			octx.c[j] = nva_rd32(ctx->cnum, 0xf680 + j * 4);
 		ectx = octx;
