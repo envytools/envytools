envytools/envytools

commit e07efbd3e235952342c7ddb4f6d1494657ea360d

demmt: pass memory dump messages to ioctl handlers
Foundation for decoding pointers embedded in ioctl structures.

rnn/mmt_bin_decode_nvidia.c
@@ -26,39 +26,159 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int load_create_object(struct mmt_nvidia_create_object **create_, int pfx)
+{
+	int size = sizeof(struct mmt_nvidia_create_object) + 1;
+	struct mmt_nvidia_create_object *create;
+	create = mmt_load_data_with_prefix(size, pfx);
+	size += create->name.len;
+	create = mmt_load_data_with_prefix(size, pfx);
+
+	mmt_check_eor(size + pfx);
+
+	if (create_)
+		*create_ = create;
+	return size;
+}
+
+#define CREATE_LOADER(name, type) \
+static int name(type **obj_, int pfx)                 \
+{                                                     \
+	int size = sizeof(type) + 1;                      \
+	type *obj = mmt_load_data_with_prefix(size, pfx); \
+                                                      \
+	mmt_check_eor(size + pfx);                        \
+                                                      \
+	if (obj_)                                         \
+		*obj_ = obj;                                  \
+	return size;                                      \
+}
+
+CREATE_LOADER(load_destroy_object, struct mmt_nvidia_destroy_object)
+CREATE_LOADER(load_call_method, struct mmt_nvidia_call_method)
+CREATE_LOADER(load_create_mapped_object, struct mmt_nvidia_create_mapped_object)
+CREATE_LOADER(load_create_dma_object, struct mmt_nvidia_create_dma_object)
+CREATE_LOADER(load_alloc_map, struct mmt_nvidia_alloc_map)
+CREATE_LOADER(load_gpu_map, struct mmt_nvidia_gpu_map)
+CREATE_LOADER(load_gpu_map2, struct mmt_nvidia_gpu_map2)
+CREATE_LOADER(load_gpu_unmap, struct mmt_nvidia_gpu_unmap)
+CREATE_LOADER(load_gpu_unmap2, struct mmt_nvidia_gpu_unmap2)
+CREATE_LOADER(load_mmap, struct mmt_nvidia_mmap)
+CREATE_LOADER(load_unmap, struct mmt_nvidia_unmap)
+CREATE_LOADER(load_bind, struct mmt_nvidia_bind)
+CREATE_LOADER(load_create_driver_object, struct mmt_nvidia_create_driver_object)
+CREATE_LOADER(load_create_device_object, struct mmt_nvidia_create_device_object)
+CREATE_LOADER(load_create_context_object, struct mmt_nvidia_create_context_object)
+
+static int load_memory_dump(int pfx, struct mmt_memory_dump_prefix **dump, struct mmt_buf **buf, struct mmt_nvidia_decode_funcs *funcs)
+{
+	int size1, size2, omitted = 0;
+	struct mmt_memory_dump_prefix *d;
+	struct mmt_buf *b;
+	*dump = NULL;
+	*buf = NULL;
+	struct mmt_message_nv *nv;
+
+	do
+	{
+		struct mmt_message *msg = mmt_load_data_with_prefix(1, pfx);
+		if (msg == NULL || msg->type != 'n')
+			return 0;
+
+		mmt_load_data_with_prefix(sizeof(struct mmt_message_nv), pfx);
+
+		nv = mmt_load_data_with_prefix(sizeof(struct mmt_message_nv), pfx);
+		if (nv == NULL)
+			return 0;
+
+		if (nv->subtype != 'o')
+		{
+			int omit;
+			if (nv->subtype == 'c' && funcs->create_object == NULL)
+				omit = load_create_object(NULL, pfx);
+			else if (nv->subtype == 'd' && funcs->destroy_object == NULL)
+				omit = load_destroy_object(NULL, pfx);
+			else if (nv->subtype == 'l' && funcs->call_method == NULL)
+				omit = load_call_method(NULL, pfx);
+			else if (nv->subtype == 'p' && funcs->create_mapped == NULL)
+				omit = load_create_mapped_object(NULL, pfx);
+			else if (nv->subtype == 't' && funcs->create_dma_object == NULL)
+				omit = load_create_dma_object(NULL, pfx);
+			else if (nv->subtype == 'a' && funcs->alloc_map == NULL)
+				omit = load_alloc_map(NULL, pfx);
+			else if (nv->subtype == 'g' && funcs->gpu_map == NULL)
+				omit = load_gpu_map(NULL, pfx);
+			else if (nv->subtype == 'G' && funcs->gpu_map2 == NULL)
+				omit = load_gpu_map2(NULL, pfx);
+			else if (nv->subtype == 'h' && funcs->gpu_unmap == NULL)
+				omit = load_gpu_unmap(NULL, pfx);
+			else if (nv->subtype == 'H' && funcs->gpu_unmap2 == NULL)
+				omit = load_gpu_unmap2(NULL, pfx);
+			else if (nv->subtype == 'm' /*&& funcs->mmap == NULL*/)
+				omit = load_mmap(NULL, pfx);
+			else if (nv->subtype == 'e' && funcs->unmap == NULL)
+				omit = load_unmap(NULL, pfx);
+			else if (nv->subtype == 'b' && funcs->bind == NULL)
+				omit = load_bind(NULL, pfx);
+			else if (nv->subtype == 'r' && funcs->create_driver_object == NULL)
+				omit = load_create_driver_object(NULL, pfx);
+			else if (nv->subtype == 'v' && funcs->create_device_object == NULL)
+				omit = load_create_device_object(NULL, pfx);
+			else if (nv->subtype == 'x' && funcs->create_context_object == NULL)
+				omit = load_create_context_object(NULL, pfx);
+			else
+			{
+				//if (nv->subtype != 'j' && nv->subtype != 'i')
+				//	printf("%d '%c'\n", nv->subtype, nv->subtype);
+				return 0;
+			}
+
+			omitted += omit;
+			pfx += omit;
+		}
+	}
+	while (nv->subtype != 'o');
+
+	size1 = sizeof(struct mmt_memory_dump_prefix);
+	d = mmt_load_data_with_prefix(size1, pfx);
+	size1 += d->str.len;
+	d = mmt_load_data_with_prefix(size1, pfx);
+
+	size2 = 4;
+	b = mmt_load_data_with_prefix(size2, size1 + pfx);
+	size2 += b->len + 1;
+	b = mmt_load_data_with_prefix(size2, size1 + pfx);
+
+	mmt_check_eor(size2 + size1 + pfx);
+
+	*dump = d;
+	*buf = b;
+
+	return size1 + size2 + omitted;
+}
+
+#define GENERATE_HANDLER(subtype_, type, loader, func) \
+		else if (nv->subtype == subtype_)     \
+		{                                     \
+			type *obj;                        \
+			size = loader(&obj, 0);           \
+                                              \
+			if (funcs->func)                  \
+				funcs->func(obj, state);      \
+                                              \
+			mmt_idx += size;                  \
+		}
+
+#define MAX_ARGS 10
 void mmt_decode_nvidia(struct mmt_nvidia_decode_funcs *funcs, void *state)
 {
 	struct mmt_message_nv *nv = mmt_load_data(sizeof(struct mmt_message_nv));
 	int size;
-	if (nv->subtype == 'c')
+
+	if (nv->subtype == 'i')
 	{
-		size = sizeof(struct mmt_nvidia_create_object) + 1;
-		struct mmt_nvidia_create_object *create;
-		create = mmt_load_data(size);
-		size += create->name.len;
-		create = mmt_load_data(size);
+		int size2, pfx;
 
-		mmt_check_eor(size);
-
-		if (funcs->create_object)
-			funcs->create_object(create, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'd')
-	{
-		size = sizeof(struct mmt_nvidia_destroy_object) + 1;
-		struct mmt_nvidia_destroy_object *destroy = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->destroy_object)
-			funcs->destroy_object(destroy, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'i')
-	{
 		size = sizeof(struct mmt_ioctl_pre) + 1;
 		struct mmt_ioctl_pre *ctl;
 		ctl = mmt_load_data(size);
@@ -67,13 +187,33 @@ void mmt_decode_nvidia(struct mmt_nvidia_decode_funcs *funcs, void *state)
 
 		mmt_check_eor(size);
 
-		if (funcs->ioctl_pre)
-			funcs->ioctl_pre(ctl, state);
+		struct mmt_memory_dump args[MAX_ARGS];
+		int argc = 0;
 
-		mmt_idx += size;
+		struct mmt_memory_dump_prefix *d;
+		struct mmt_buf *b;
+		pfx = size;
+
+		while ((size2 = load_memory_dump(pfx, &d, &b, funcs)))
+		{
+			args[argc].addr = d->addr;
+			args[argc].str = &d->str;
+			args[argc].data = b;
+			argc++;
+			pfx += size2;
+			if (argc == MAX_ARGS)
+				break;
+		}
+
+		if (funcs->ioctl_pre)
+			funcs->ioctl_pre(ctl, state, args, argc);
+
+		mmt_idx += pfx;
 	}
 	else if (nv->subtype == 'j')
 	{
+		int size2, pfx;
+
 		size = sizeof(struct mmt_ioctl_post) + 1;
 		struct mmt_ioctl_post *ctl;
 		ctl = mmt_load_data(size);
@@ -82,205 +222,57 @@ void mmt_decode_nvidia(struct mmt_nvidia_decode_funcs *funcs, void *state)
 
 		mmt_check_eor(size);
 
-		if (funcs->ioctl_post)
-			funcs->ioctl_post(ctl, state);
+		struct mmt_memory_dump args[MAX_ARGS];
+		int argc = 0;
 
-		mmt_idx += size;
+		struct mmt_memory_dump_prefix *d;
+		struct mmt_buf *b;
+		pfx = size;
+
+		while ((size2 = load_memory_dump(pfx, &d, &b, funcs)))
+		{
+			args[argc].addr = d->addr;
+			args[argc].str = &d->str;
+			args[argc].data = b;
+			argc++;
+			pfx += size2;
+			if (argc == MAX_ARGS)
+				break;
+		}
+
+		if (funcs->ioctl_post)
+			funcs->ioctl_post(ctl, state, args, argc);
+
+		mmt_idx += pfx;
 	}
 	else if (nv->subtype == 'o')
 	{
-		size = sizeof(struct mmt_nvidia_memory_dump);
-		struct mmt_nvidia_memory_dump *d;
-		d = mmt_load_data(size);
-		size += d->str.len;
-		d = mmt_load_data(size);
+		struct mmt_memory_dump_prefix *d;
+		struct mmt_buf *b;
+
+		size = load_memory_dump(0, &d, &b, funcs);
 
 		if (funcs->memory_dump)
-			funcs->memory_dump(d, state);
-
-		mmt_idx += size;
-
-		struct mmt_buf *b;
-		size = 4;
-		b = mmt_load_data(size);
-		size += b->len + 1;
-		b = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->memory_dump_cont)
-			funcs->memory_dump_cont(b, state);
+			funcs->memory_dump(d, b, state);
 
 		mmt_idx += size;
 	}
-	else if (nv->subtype == 'l')
-	{
-		size = sizeof(struct mmt_nvidia_call_method) + 1;
-		struct mmt_nvidia_call_method *m = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->call_method)
-			funcs->call_method(m, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'p')
-	{
-		size = sizeof(struct mmt_nvidia_create_mapped_object) + 1;
-		struct mmt_nvidia_create_mapped_object *p = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->create_mapped)
-			funcs->create_mapped(p, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 't')
-	{
-		size = sizeof(struct mmt_nvidia_create_dma_object) + 1;
-		struct mmt_nvidia_create_dma_object *create = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->create_dma_object)
-			funcs->create_dma_object(create, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'a')
-	{
-		size = sizeof(struct mmt_nvidia_alloc_map) + 1;
-		struct mmt_nvidia_alloc_map *alloc = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->alloc_map)
-			funcs->alloc_map(alloc, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'g')
-	{
-		size = sizeof(struct mmt_nvidia_gpu_map) + 1;
-		struct mmt_nvidia_gpu_map *map = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->gpu_map)
-			funcs->gpu_map(map, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'G')
-	{
-		size = sizeof(struct mmt_nvidia_gpu_map2) + 1;
-		struct mmt_nvidia_gpu_map2 *map = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->gpu_map2)
-			funcs->gpu_map2(map, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'h')
-	{
-		size = sizeof(struct mmt_nvidia_gpu_unmap) + 1;
-		struct mmt_nvidia_gpu_unmap *unmap = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->gpu_unmap)
-			funcs->gpu_unmap(unmap, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'H')
-	{
-		size = sizeof(struct mmt_nvidia_gpu_unmap2) + 1;
-		struct mmt_nvidia_gpu_unmap2 *unmap = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->gpu_unmap2)
-			funcs->gpu_unmap2(unmap, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'm')
-	{
-		size = sizeof(struct mmt_nvidia_mmap) + 1;
-		struct mmt_nvidia_mmap *map = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->mmap)
-			funcs->mmap(map, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'e')
-	{
-		size = sizeof(struct mmt_nvidia_unmap) + 1;
-		struct mmt_nvidia_unmap *unmap = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->unmap)
-			funcs->unmap(unmap, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'b')
-	{
-		size = sizeof(struct mmt_nvidia_bind) + 1;
-		struct mmt_nvidia_bind *bnd = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->bind)
-			funcs->bind(bnd, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'r')
-	{
-		size = sizeof(struct mmt_nvidia_create_driver_object) + 1;
-		struct mmt_nvidia_create_driver_object *create = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->create_driver_object)
-			funcs->create_driver_object(create, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'v')
-	{
-		size = sizeof(struct mmt_nvidia_create_device_object) + 1;
-		struct mmt_nvidia_create_device_object *create = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->create_device_object)
-			funcs->create_device_object(create, state);
-
-		mmt_idx += size;
-	}
-	else if (nv->subtype == 'x')
-	{
-		size = sizeof(struct mmt_nvidia_create_context_object) + 1;
-		struct mmt_nvidia_create_context_object *create = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
-		if (funcs->create_context_object)
-			funcs->create_context_object(create, state);
-
-		mmt_idx += size;
-	}
+	GENERATE_HANDLER('c', struct mmt_nvidia_create_object,         load_create_object,         create_object)
+	GENERATE_HANDLER('d', struct mmt_nvidia_destroy_object,        load_destroy_object,        destroy_object)
+	GENERATE_HANDLER('l', struct mmt_nvidia_call_method,           load_call_method,           call_method)
+	GENERATE_HANDLER('p', struct mmt_nvidia_create_mapped_object,  load_create_mapped_object,  create_mapped)
+	GENERATE_HANDLER('t', struct mmt_nvidia_create_dma_object,     load_create_dma_object,     create_dma_object)
+	GENERATE_HANDLER('a', struct mmt_nvidia_alloc_map,             load_alloc_map,             alloc_map)
+	GENERATE_HANDLER('g', struct mmt_nvidia_gpu_map,               load_gpu_map,               gpu_map)
+	GENERATE_HANDLER('G', struct mmt_nvidia_gpu_map2,              load_gpu_map2,              gpu_map2)
+	GENERATE_HANDLER('h', struct mmt_nvidia_gpu_unmap,             load_gpu_unmap,             gpu_unmap)
+	GENERATE_HANDLER('H', struct mmt_nvidia_gpu_unmap2,            load_gpu_unmap2,            gpu_unmap2)
+	GENERATE_HANDLER('m', struct mmt_nvidia_mmap,                  load_mmap,                  mmap)
+	GENERATE_HANDLER('b', struct mmt_nvidia_bind,                  load_bind,                  bind)
+	GENERATE_HANDLER('e', struct mmt_nvidia_unmap,                 load_unmap,                 unmap)
+	GENERATE_HANDLER('r', struct mmt_nvidia_create_driver_object,  load_create_driver_object,  create_driver_object)
+	GENERATE_HANDLER('v', struct mmt_nvidia_create_device_object,  load_create_device_object,  create_device_object)
+	GENERATE_HANDLER('x', struct mmt_nvidia_create_context_object, load_create_context_object, create_context_object)
 	else if (nv->subtype == '1')
 	{
 		struct mmt_nvidia_call_method_data *call;

rnn/demmt.c
@@ -990,7 +990,7 @@ static void decode_ioctl_id(uint32_t id, uint8_t *dir, uint8_t *type, uint8_t *n
 
 static char *dir_desc[] = { "?", "w", "r", "rw" };
 
-void demmt_ioctl_pre(struct mmt_ioctl_pre *ctl, void *state)
+void demmt_ioctl_pre(struct mmt_ioctl_pre *ctl, void *state, struct mmt_memory_dump *args, int argc)
 {
 	uint8_t dir, type, nr;
 	uint16_t size;
@@ -1003,7 +1003,7 @@ void demmt_ioctl_pre(struct mmt_ioctl_pre *ctl, void *state)
 		print_raw = demmt_drm_ioctl_pre(dir, nr, size, &ctl->data, state);
 	}
 	else if (type == 0x46) // nvidia
-		print_raw = demmt_nv_ioctl_pre(ctl->id, dir, nr, size, &ctl->data, state);
+		print_raw = demmt_nv_ioctl_pre(ctl->id, dir, nr, size, &ctl->data, state, args, argc);
 
 	print_raw = print_raw || dump_ioctls;
 
@@ -1035,7 +1035,7 @@ void demmt_ioctl_pre(struct mmt_ioctl_pre *ctl, void *state)
 		mmt_log_cont("%s\n", "");
 }
 
-void demmt_ioctl_post(struct mmt_ioctl_post *ctl, void *state)
+void demmt_ioctl_post(struct mmt_ioctl_post *ctl, void *state, struct mmt_memory_dump *args, int argc)
 {
 	uint8_t dir, type, nr;
 	uint16_t size;
@@ -1045,7 +1045,7 @@ void demmt_ioctl_post(struct mmt_ioctl_post *ctl, void *state)
 	if (type == 0x64) // DRM
 		print_raw = demmt_drm_ioctl_post(dir, nr, size, &ctl->data, state);
 	else if (type == 0x46) // nvidia
-		print_raw = demmt_nv_ioctl_post(ctl->id, dir, nr, size, &ctl->data, state);
+		print_raw = demmt_nv_ioctl_post(ctl->id, dir, nr, size, &ctl->data, state, args, argc);
 
 	print_raw = print_raw || dump_ioctls;
 
@@ -1087,8 +1087,7 @@ const struct mmt_nvidia_decode_funcs demmt_funcs =
 	NULL,
 	demmt_ioctl_pre,
 	demmt_ioctl_post,
-	demmt_nv_memory_dump,
-	demmt_nv_memory_dump_cont,
+	demmt_memory_dump,
 	NULL,
 	NULL,
 	NULL,

rnn/demmt_nvrm.c
@@ -509,7 +509,8 @@ static void handle_nvrm_ioctl_bind(struct nvrm_ioctl_bind *s)
 		mmt_log("bind: target: 0x%08x, handle: 0x%08x\n", s->target, s->handle);
 }
 
-int demmt_nv_ioctl_pre(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size, struct mmt_buf *buf, void *state)
+int demmt_nv_ioctl_pre(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size,
+		struct mmt_buf *buf, void *state, struct mmt_memory_dump *args, int argc)
 {
 	int k, found = 0;
 	for (k = 0; k < ARRAY_SIZE(ioctls); ++k)
@@ -544,7 +545,8 @@ int demmt_nv_ioctl_pre(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size, stru
 	return 0;
 }
 
-int demmt_nv_ioctl_post(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size, struct mmt_buf *buf, void *state)
+int demmt_nv_ioctl_post(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size,
+		struct mmt_buf *buf, void *state, struct mmt_memory_dump *args, int argc)
 {
 	int k, found = 0;
 	for (k = 0; k < ARRAY_SIZE(ioctls); ++k)
@@ -581,12 +583,15 @@ int demmt_nv_ioctl_post(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size, str
 	return 0;
 }
 
-void demmt_nv_memory_dump(struct mmt_nvidia_memory_dump *d, void *state)
+void demmt_memory_dump(struct mmt_memory_dump_prefix *d, struct mmt_buf *b, void *state)
 {
-}
+	// dead code, because memory dumps are passed to ioctl_pre / ioctl_post handlers
+	int i;
+	mmt_log("memory dump, addr: 0x%016lx, txt: \"%s\", data.len: %d, data:", d->addr, d->str.data, b->len);
 
-void demmt_nv_memory_dump_cont(struct mmt_buf *b, void *state)
-{
+	for (i = 0; i < b->len / 4; ++i)
+		mmt_log_cont(" 0x%08x", ((uint32_t *)b->data)[i]);
+	mmt_log_cont("%s", "\n");
 }
 
 void demmt_nv_mmap(struct mmt_nvidia_mmap *mm, void *state)

rnn/demmt_nvrm.h
@@ -3,12 +3,12 @@
 
 #include "mmt_bin_decode_nvidia.h"
 
-int demmt_nv_ioctl_pre(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size, struct mmt_buf *buf, void *state);
-int demmt_nv_ioctl_post(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size, struct mmt_buf *buf, void *state);
+int demmt_nv_ioctl_pre(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size,
+		struct mmt_buf *buf, void *state, struct mmt_memory_dump *args, int argc);
+int demmt_nv_ioctl_post(uint32_t id, uint8_t dir, uint8_t nr, uint16_t size,
+		struct mmt_buf *buf, void *state, struct mmt_memory_dump *args, int argc);
 
-void demmt_ioctl_post(struct mmt_ioctl_post *ctl, void *state);
-void demmt_nv_memory_dump(struct mmt_nvidia_memory_dump *d, void *state);
-void demmt_nv_memory_dump_cont(struct mmt_buf *b, void *state);
+void demmt_memory_dump(struct mmt_memory_dump_prefix *d, struct mmt_buf *b, void *state);
 void demmt_nv_mmap(struct mmt_nvidia_mmap *mm, void *state);
 void demmt_nv_call_method_data(struct mmt_nvidia_call_method_data *call, void *state);
 void demmt_nv_ioctl_4d(struct mmt_nvidia_ioctl_4d *ctl, void *state);

rnn/mmt_bin2dedma.h
@@ -16,7 +16,6 @@ void txt_msg(uint8_t *data, int len, void *state);
 
 struct mmt_txt_nvidia_state
 {
-	int memdump_pfx_printed;
 } mmt_txt_nv_state;
 
 extern const struct mmt_nvidia_decode_funcs txt_nvidia_funcs;

rnn/mmt_bin2dedma_nvidia.c
@@ -31,7 +31,23 @@
 #include <nouveau_drm.h>
 #endif
 
-static void txt_nv_ioctl_pre(struct mmt_ioctl_pre *ctl, void *state)
+static void dump_args(struct mmt_memory_dump *args, int argc)
+{
+	int i, j;
+
+	for (i = 0; i < argc; ++i)
+	{
+		struct mmt_memory_dump *arg = &args[i];
+		struct mmt_buf *data = arg->data;
+		fprintf(stdout, PFX "address: 0x%llx, txt: \"%s\" data.len: %d, data:",
+				(unsigned long long)arg->addr, arg->str->data, data->len);
+		for (j = 0; j < data->len / 4; ++j)
+			fprintf(stdout, " 0x%08x", ((uint32_t *)data->data)[j]);
+		fprintf(stdout, "\n");
+	}
+}
+
+static void txt_nv_ioctl_pre(struct mmt_ioctl_pre *ctl, void *state, struct mmt_memory_dump *args, int argc)
 {
 	uint32_t i;
 	fprintf(stdout, PFX "pre_ioctl:  fd:%d, id:0x%02x (full:0x%x), data: ", ctl->fd, ctl->id & 0xFF, ctl->id);
@@ -67,9 +83,11 @@ static void txt_nv_ioctl_pre(struct mmt_ioctl_pre *ctl, void *state)
 		fprintf(stdout, PFX "create gpu object 0x%08x:0x%08x type 0x%04x (%s)\n", 0, s->handle, s->class, "");
 	}
 #endif
+
+	dump_args(args, argc);
 }
 
-static void txt_nv_ioctl_post(struct mmt_ioctl_post *ctl, void *state)
+static void txt_nv_ioctl_post(struct mmt_ioctl_post *ctl, void *state, struct mmt_memory_dump *args, int argc)
 {
 	uint32_t i;
 	fprintf(stdout, PFX "post_ioctl: fd:%d, id:0x%02x (full:0x%x), data: ", ctl->fd, ctl->id & 0xFF, ctl->id);
@@ -135,37 +153,25 @@ static void txt_nv_ioctl_post(struct mmt_ioctl_post *ctl, void *state)
 		struct nvrm_ioctl_create_ctx *s = d;
 		fprintf(stdout, PFX "created context object 0x%08x\n", s->handle);
 	}
+
+	dump_args(args, argc);
 }
 
-static void txt_nv_memory_dump(struct mmt_nvidia_memory_dump *d, void *state)
+static void txt_memory_dump(struct mmt_memory_dump_prefix *d, struct mmt_buf *b, void *state)
 {
 	if (d->str.len == 0)
 		return;
-	struct mmt_txt_nvidia_state *nstate = state;
 
 	fprintf(stdout, PFX "%s", d->str.data);
-	nstate->memdump_pfx_printed = 1;
-}
-
-static void txt_nv_memory_dump_cont(struct mmt_buf *b, void *state)
-{
-	struct mmt_txt_nvidia_state *nstate = state;
 
 	if (b->len > 0)
 	{
-		if (!nstate->memdump_pfx_printed)
-		{
-			fprintf(stdout, PFX);
-			nstate->memdump_pfx_printed = 1;
-		}
 		uint32_t i;
 		for (i = 0; i < b->len / 4; ++i)
 			fprintf(stdout, "0x%08x ", ((uint32_t *)b->data)[i]);
 	}
 
-	if (nstate->memdump_pfx_printed)
-		fprintf(stdout, "\n");
-	nstate->memdump_pfx_printed = 0;
+	fprintf(stdout, "\n");
 }
 
 static void txt_nv_mmap(struct mmt_nvidia_mmap *map, void *state)
@@ -208,8 +214,7 @@ const struct mmt_nvidia_decode_funcs txt_nvidia_funcs =
 	NULL,
 	txt_nv_ioctl_pre,
 	txt_nv_ioctl_post,
-	txt_nv_memory_dump,
-	txt_nv_memory_dump_cont,
+	txt_memory_dump,
 	NULL,
 	NULL,
 	NULL,
@@ -233,7 +238,6 @@ const struct mmt_nvidia_decode_funcs txt_nvidia_funcs =
 const struct mmt_nvidia_decode_funcs txt_nvidia_funcs_empty =
 {
 	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL },
-	NULL,
 	NULL,
 	NULL,
 	NULL,

rnn/mmt_bin_decode.c
@@ -38,13 +38,13 @@ static int len = 0;
 
 #define EOR 10
 
-void *mmt_load_data(int sz)
+void *mmt_load_data_with_prefix(int sz, int pfx)
 {
-	if (mmt_idx + sz < len)
-		return mmt_buf + mmt_idx;
-	if (sz > MMT_BUF_SIZE)
+	if (pfx + mmt_idx + sz < len)
+		return mmt_buf + pfx + mmt_idx;
+	if (pfx + sz > MMT_BUF_SIZE)
 	{
-		fprintf(stderr, "not enough space for message of size %d\n", sz);
+		fprintf(stderr, "not enough space for message of size %d\n", pfx + sz);
 		fflush(stderr);
 		exit(1);
 	}
@@ -56,7 +56,7 @@ void *mmt_load_data(int sz)
 		mmt_idx = 0;
 	}
 
-	while (mmt_idx + sz >= len)
+	while (pfx + mmt_idx + sz >= len)
 	{
 		int r = read(0, mmt_buf + len, MMT_BUF_SIZE - len);
 		if (r < 0)
@@ -73,7 +73,12 @@ void *mmt_load_data(int sz)
 		len += r;
 	}
 
-	return mmt_buf + mmt_idx;
+	return mmt_buf + pfx + mmt_idx;
+}
+
+void *mmt_load_data(int sz)
+{
+	return mmt_load_data_with_prefix(sz, 0);
 }
 
 void mmt_dump_next()

rnn/mmt_bin_decode.h
@@ -9,6 +9,7 @@ extern int mmt_idx;
 
 void mmt_check_eor(int sz);
 void *mmt_load_data(int sz);
+void *mmt_load_data_with_prefix(int sz, int pfx);
 void mmt_dump_next();
 
 #define __packed  __attribute__((__packed__))

commit fa905b8d7fc4733594a4365503b03ec3d1a76fc9

demmt: fix processing of ioctl messages on buffer boundary
broken by commit e07efbd3e235952342c7ddb4f6d1494657ea360d
"demmt: pass memory dump messages to ioctl handlers"

rnn/mmt_bin_decode_nvidia.c
@@ -178,32 +178,37 @@ void mmt_decode_nvidia(struct mmt_nvidia_decode_funcs *funcs, void *state)
 	if (nv->subtype == 'i')
 	{
 		int size2, pfx;
-
-		size = sizeof(struct mmt_ioctl_pre) + 1;
-		struct mmt_ioctl_pre *ctl;
-		ctl = mmt_load_data(size);
-		size += ctl->data.len;
-		ctl = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
 		struct mmt_memory_dump args[MAX_ARGS];
-		int argc = 0;
+		struct mmt_ioctl_pre *ctl;
+		int argc;
 
-		struct mmt_memory_dump_prefix *d;
-		struct mmt_buf *b;
-		pfx = size;
-
-		while ((size2 = load_memory_dump(pfx, &d, &b, funcs)))
+		do
 		{
-			args[argc].addr = d->addr;
-			args[argc].str = &d->str;
-			args[argc].data = b;
-			argc++;
-			pfx += size2;
-			if (argc == MAX_ARGS)
-				break;
+			size = sizeof(struct mmt_ioctl_pre) + 1;
+			ctl = mmt_load_data(size);
+			size += ctl->data.len;
+			ctl = mmt_load_data(size);
+
+			mmt_check_eor(size);
+
+			argc = 0;
+
+			struct mmt_memory_dump_prefix *d;
+			struct mmt_buf *b;
+			pfx = size;
+
+			while ((size2 = load_memory_dump(pfx, &d, &b, funcs)))
+			{
+				args[argc].addr = d->addr;
+				args[argc].str = &d->str;
+				args[argc].data = b;
+				argc++;
+				pfx += size2;
+				if (argc == MAX_ARGS)
+					break;
+			}
 		}
+		while (ctl != mmt_load_data(size));
 
 		if (funcs->ioctl_pre)
 			funcs->ioctl_pre(ctl, state, args, argc);
@@ -213,32 +218,37 @@ void mmt_decode_nvidia(struct mmt_nvidia_decode_funcs *funcs, void *state)
 	else if (nv->subtype == 'j')
 	{
 		int size2, pfx;
-
-		size = sizeof(struct mmt_ioctl_post) + 1;
-		struct mmt_ioctl_post *ctl;
-		ctl = mmt_load_data(size);
-		size += ctl->data.len;
-		ctl = mmt_load_data(size);
-
-		mmt_check_eor(size);
-
 		struct mmt_memory_dump args[MAX_ARGS];
-		int argc = 0;
+		struct mmt_ioctl_post *ctl;
+		int argc;
 
-		struct mmt_memory_dump_prefix *d;
-		struct mmt_buf *b;
-		pfx = size;
-
-		while ((size2 = load_memory_dump(pfx, &d, &b, funcs)))
+		do
 		{
-			args[argc].addr = d->addr;
-			args[argc].str = &d->str;
-			args[argc].data = b;
-			argc++;
-			pfx += size2;
-			if (argc == MAX_ARGS)
-				break;
+			size = sizeof(struct mmt_ioctl_post) + 1;
+			ctl = mmt_load_data(size);
+			size += ctl->data.len;
+			ctl = mmt_load_data(size);
+
+			mmt_check_eor(size);
+
+			argc = 0;
+
+			struct mmt_memory_dump_prefix *d;
+			struct mmt_buf *b;
+			pfx = size;
+
+			while ((size2 = load_memory_dump(pfx, &d, &b, funcs)))
+			{
+				args[argc].addr = d->addr;
+				args[argc].str = &d->str;
+				args[argc].data = b;
+				argc++;
+				pfx += size2;
+				if (argc == MAX_ARGS)
+					break;
+			}
 		}
+		while (ctl != mmt_load_data(size));
 
 		if (funcs->ioctl_post)
 			funcs->ioctl_post(ctl, state, args, argc);