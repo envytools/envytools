30ef233d89c2092729c6472f9f879faa347f14e1
Marcin Ślusarz <marcin.slusarz@gmail.com>
2014-11-24 00:14:43 +0100

    demmt: fix build without libdrm_nouveau

--- demmt/drm.h
+++ demmt/drm.h
@@ -11,12 +11,12 @@ int demmt_drm_ioctl_post(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size,
 		struct mmt_memory_dump *args, int argc);
 void demmt_nouveau_gem_pushbuf_data(struct mmt_nouveau_pushbuf_data *data, void *state);
 #else
-static inline int demmt_drm_ioctl_pre(uint8_t dir, uint8_t nr, uint16_t size,
+static inline int demmt_drm_ioctl_pre(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size,
 		struct mmt_buf *data, void *state, struct mmt_memory_dump *args, int argc)
 {
 	return 1;
 }
-static inline int demmt_drm_ioctl_post(uint8_t dir, uint8_t nr, uint16_t size,
+static inline int demmt_drm_ioctl_post(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size,
 		struct mmt_buf *data, uint64_t ret, uint64_t err, void *state,
 		struct mmt_memory_dump *args, int argc)
 {
