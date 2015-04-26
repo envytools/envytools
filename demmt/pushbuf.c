envytools/envytools

commit f5f2659cf22c50458a37b4a0337620ac21b658a2

demmt: multi-device/context support and automatic chipset detection
Yay, no more static data describing gpu state.

Notes:
- it works only on traces obtained by mmt built after 7 Sep,
- it works only for traces which use IBs (which is tesla+, but without some
  modesetting traces which use old-style (pre-nv50) command submission)
- it leaks memory (object state) on ioctl_destroy,
- nouveau support is a bunch of hacks

demmt/pushbuf.c
@@ -27,15 +27,35 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "rnndec.h"
-#include "util.h"
 #include "buffer.h"
 #include "config.h"
 #include "demmt.h"
-#include "pushbuf.h"
-#include "object_state.h"
-#include "nvrm_decode.h"
 #include "log.h"
+#include "nvrm.h"
+#include "nvrm_decode.h"
+#include "nvrm_object.xml.h"
+#include "object_state.h"
+#include "pushbuf.h"
+#include "rnndec.h"
+#include "util.h"
+
+struct fifo_state *get_fifo_state(struct gpu_object *fifo)
+{
+	if (fifo->class_data == NULL)
+		fifo->class_data = calloc(1, sizeof(struct fifo_state));
+	return fifo->class_data;
+}
+
+struct obj **get_subchans(struct pushbuf_decode_state *pstate)
+{
+	return get_fifo_state(pstate->fifo)->subchans;
+}
+
+struct obj *current_subchan_object(struct pushbuf_decode_state *pstate)
+{
+	struct obj **subchans = get_subchans(pstate);
+	return subchans[pstate->subchan];
+}
 
 uint32_t pb_pointer_buffer = UINT32_MAX;
 uint32_t pb_pointer_offset = 0;
@@ -46,11 +66,15 @@ void pushbuf_decode_start(struct pushbuf_decode_state *state)
 	memset(state, 0, sizeof(*state));
 }
 
-struct obj *subchans[8] = { NULL };
-#define MAX_OBJECTS 256
-static struct obj objects[MAX_OBJECTS];
+static inline struct obj *get_all_objects(struct gpu_object *gpu_obj)
+{
+	struct gpu_object *fifo = nvrm_get_parent_fifo(gpu_obj);
+	if (!fifo)
+		return NULL;
+	return get_fifo_state(fifo)->objects;
+}
 
-void pushbuf_add_object(uint32_t handle, uint32_t class)
+void pushbuf_add_object(uint32_t handle, uint32_t class, struct gpu_object *gpu_obj)
 {
 	struct rnnenum *chs = rnn_findenum(rnndb, "chipset");
 	struct rnnenum *cls = rnn_findenum(rnndb, "obj-class");
@@ -65,6 +89,13 @@ void pushbuf_add_object(uint32_t handle, uint32_t class)
 		abort();
 	}
 
+	if (class == NVRM_DEVICE_0 || class == NVRM_SUBDEVICE_0)
+		return;
+
+	struct obj *objects = get_all_objects(gpu_obj);
+	if (!objects)
+		return;
+
 	for (i = 0; obj = &objects[i], i < MAX_OBJECTS; i++)
 	{
 		if (obj->handle)
@@ -76,9 +107,12 @@ void pushbuf_add_object(uint32_t handle, uint32_t class)
 		obj->ctx = rnndec_newcontext(rnndb);
 		obj->ctx->colors = colors;
 		obj->decoder = demmt_get_decoder(class);
+		obj->gpu_object = gpu_obj;
+		if (obj->decoder)
+			obj->decoder->init(gpu_obj);
 
 		v = NULL;
-		FINDARRAY(chs->vals, v, v->value == (uint64_t)chipset);
+		FINDARRAY(chs->vals, v, v->value == (uint64_t)nvrm_get_chipset(gpu_obj));
 		rnndec_varadd(obj->ctx, "chipset", v ? v->name : "NV1");
 
 		v = NULL;
@@ -94,9 +128,9 @@ void pushbuf_add_object(uint32_t handle, uint32_t class)
 	abort();
 }
 
-void pushbuf_add_object_name(uint32_t handle, uint32_t name)
+void pushbuf_add_object_name(uint32_t handle, uint32_t name, struct gpu_object *gpu_obj)
 {
-	struct obj *objs = objects;
+	struct obj *objs = get_all_objects(gpu_obj);
 	int i;
 	for (i = 0; i < MAX_OBJECTS; i++)
 		if (objs[i].handle == handle)
@@ -108,9 +142,9 @@ void pushbuf_add_object_name(uint32_t handle, uint32_t name)
 	mmt_error("pushbuf_add_object_name(0x%08x, 0x%08x): no object\n", handle, name);
 }
 
-static struct obj *get_object(uint32_t handle)
+static struct obj *get_object(uint32_t handle, struct gpu_object *gpu_obj)
 {
-	struct obj *objs = objects;
+	struct obj *objs = get_all_objects(gpu_obj);
 	int i;
 	if (handle == 0)
 		return NULL;
@@ -123,12 +157,21 @@ static struct obj *get_object(uint32_t handle)
 		if (objs[i].name == handle)
 			return &objs[i];
 
-	if (chipset >= 0xc0)
+	if (nvrm_get_chipset(gpu_obj) >= 0xc0)
 	{
-		if (!is_nouveau)
+		if (is_nouveau)
+		{
+			// hack
+			struct gpu_object *gpu_obj2 = gpu_object_add(gpu_obj->fd, gpu_obj->cid, 0xf1f0eeee, handle, handle & 0xffff);
+			pushbuf_add_object(handle, handle & 0xffff, gpu_obj2);
+		}
+		else
+		{
 			mmt_error("Guessing handle 0x%08x, driver forgot to call NVRM_MTHD_FIFO_IB_OBJECT_INFO?\n", handle);
-		pushbuf_add_object(handle, handle & 0xffff);
-		return get_object(handle);
+			pushbuf_add_object(handle, handle & 0xffff, gpu_obj);
+		}
+
+		return get_object(handle, gpu_obj);
 	}
 
 	return NULL;
@@ -136,7 +179,7 @@ static struct obj *get_object(uint32_t handle)
 
 static void decode_header(struct pushbuf_decode_state *state, char *output)
 {
-	struct obj *obj = subchans[state->subchan];
+	struct obj *obj = current_subchan_object(state);
 	uint32_t handle = obj ? obj->handle : 0;
 	const char *incr = state->incr ? "increment" : "constant";
 	char subchannel_desc[128];
@@ -151,7 +194,6 @@ static void decode_header(struct pushbuf_decode_state *state, char *output)
 	}
 	else
 		subchannel_desc[0] = 0;
-
 
 	if (!state->long_command)
 		sprintf(output, "size %d, subchannel %d%s, offset 0x%04x, %s",
@@ -213,7 +255,7 @@ void decode_method_raw(int mthd, uint32_t data, struct obj *obj, char *dec_obj,
 
 static void decode_method(struct pushbuf_decode_state *state, char *output)
 {
-	struct obj *obj = subchans[state->subchan];
+	struct obj *obj = current_subchan_object(state);
 	static char dec_obj[1000], dec_mthd[1000], dec_val[1000];
 
 	decode_method_raw(state->mthd, state->mthd_data, obj, dec_obj, dec_mthd, dec_val);
@@ -236,6 +278,8 @@ uint64_t pushbuf_decode(struct pushbuf_decode_state *state, uint32_t data, char 
 		state->skip--;
 		return 0;
 	}
+	struct obj **subchans = get_subchans(state);
+	int chipset = nvrm_get_chipset(state->fifo);
 
 	if (state->size == 0)
 	{
@@ -429,7 +473,7 @@ uint64_t pushbuf_decode(struct pushbuf_decode_state *state, uint32_t data, char 
 			}
 
 			if (handle)
-				subchans[state->subchan] = get_object(handle);
+				subchans[state->subchan] = get_object(handle, state->fifo);
 		}
 		if (subchans[state->subchan] == NULL && state->addr != 0 && state->pushbuf_invalid == 0)
 		{
@@ -450,14 +494,14 @@ uint64_t pushbuf_decode(struct pushbuf_decode_state *state, uint32_t data, char 
 				if (state->pushbuf_invalid)
 					mmt_log("this is invalid buffer, not going to bind object 0x%08x to subchannel %d\n", data, state->subchan);
 				else
-					subchans[state->subchan] = get_object(data);
+					subchans[state->subchan] = get_object(data, state->fifo);
 			}
 			else
 			{
 				if (data != subchans[state->subchan]->handle)
 				{
 					if (safe)
-						subchans[state->subchan] = get_object(data);
+						subchans[state->subchan] = get_object(data, state->fifo);
 					else
 					{
 						if (state->pushbuf_invalid == 0)
@@ -513,7 +557,7 @@ static uint64_t __pushbuf_print(struct pushbuf_decode_state *pstate, uint32_t *c
 		if (decode_pb)
 			fprintf(stdout, "PB: 0x%08x %s", cmd, cmdoutput);
 
-		struct obj *obj = subchans[pstate->subchan];
+		struct obj *obj = current_subchan_object(pstate);
 
 		if (obj)
 		{
@@ -528,7 +572,7 @@ static uint64_t __pushbuf_print(struct pushbuf_decode_state *pstate, uint32_t *c
 					mmt_log("not enough space for object data 0x%x\n", pstate->mthd);
 
 				if (obj->decoder && obj->decoder->decode_terse)
-					obj->decoder->decode_terse(pstate);
+					obj->decoder->decode_terse(obj->gpu_object, pstate);
 			}
 		}
 
@@ -536,7 +580,7 @@ static uint64_t __pushbuf_print(struct pushbuf_decode_state *pstate, uint32_t *c
 			fprintf(stdout, "\n");
 
 		if (pstate->mthd_data_available && obj && obj->decoder && obj->decoder->decode_verbose)
-			obj->decoder->decode_verbose(pstate);
+			obj->decoder->decode_verbose(obj->gpu_object, pstate);
 
 		cur++;
 	}
@@ -594,7 +638,7 @@ void ib_decode(struct ib_decode_state *state, uint32_t data, char *output)
 		if (state->unk8)
 			strcat(output, ", unk8");
 
-		struct gpu_mapping *gpu_mapping = gpu_mapping_find(state->address);
+		struct gpu_mapping *gpu_mapping = gpu_mapping_find(state->address, nvrm_get_device(state->pstate.fifo));
 
 		state->gpu_mapping = gpu_mapping;
 

demmt/buffer.c
@@ -27,6 +27,7 @@
 #include "buffer.h"
 #include "buffer_decode.h"
 #include "log.h"
+#include "nvrm.h"
 
 struct gpu_object *gpu_objects = NULL;
 struct cpu_mapping *cpu_mappings[MAX_ID] = { NULL };
@@ -131,7 +132,7 @@ struct gpu_object *gpu_object_find(uint32_t cid, uint32_t handle)
 	return NULL;
 }
 
-struct gpu_mapping *gpu_mapping_find(uint64_t address)
+struct gpu_mapping *gpu_mapping_find(uint64_t address, struct gpu_object *dev)
 {
 	if (address == 0)
 		return NULL;
@@ -139,9 +140,13 @@ struct gpu_mapping *gpu_mapping_find(uint64_t address)
 	struct gpu_object *obj;
 	struct gpu_mapping *gpu_mapping;
 	for (obj = gpu_objects; obj != NULL; obj = obj->next)
+	{
+		if (nvrm_get_device(obj) != dev)
+			continue;
 		for (gpu_mapping = obj->gpu_mappings; gpu_mapping != NULL; gpu_mapping = gpu_mapping->next)
 			if (address >= gpu_mapping->address && address < gpu_mapping->address + gpu_mapping->length)
 				return gpu_mapping;
+	}
 	return NULL;
 }
 

demmt/buffer.h
@@ -86,6 +86,8 @@ struct gpu_object
 		uint64_t address;
 	}
 	usage[MAX_USAGES];
+
+	void *class_data;
 };
 
 extern struct gpu_object *gpu_objects;
@@ -103,7 +105,7 @@ struct gpu_object *gpu_object_add(uint32_t fd, uint32_t cid, uint32_t parent, ui
 struct gpu_object *gpu_object_find(uint32_t cid, uint32_t handle);
 void gpu_object_destroy(struct gpu_object *obj);
 
-struct gpu_mapping *gpu_mapping_find(uint64_t address);
+struct gpu_mapping *gpu_mapping_find(uint64_t address, struct gpu_object *dev);
 void *gpu_mapping_get_data(struct gpu_mapping *mapping, uint64_t address, uint64_t length);
 void gpu_mapping_destroy(struct gpu_mapping *gpu_mapping);
 

demmt/buffer_decode.c
@@ -25,6 +25,7 @@
 #include <stdio.h>
 
 #include "buffer_decode.h"
+#include "nvrm.h"
 #include "config.h"
 #include "log.h"
 
@@ -36,6 +37,9 @@ void flush_written_regions(struct cpu_mapping *mapping)
 	comment[0][0] = 0;
 	comment[1][0] = 0;
 	pushbuf_desc[0] = 0;
+	struct gpu_object *dev = nvrm_get_device(mapping->object);
+	int chipset = nvrm_get_chipset(dev);
+	int ib_supported = chipset == 0x50 || chipset >= 0x80;
 
 	if (find_pb_pointer || (!is_nouveau && pb_pointer_buffer == UINT32_MAX))
 	{
@@ -43,7 +47,7 @@ void flush_written_regions(struct cpu_mapping *mapping)
 
 		while (cur)
 		{
-			if (ib_supported) // TODO: based on parent
+			if (ib_supported)
 			{
 				if (cur->end - cur->start < 8)
 				{
@@ -105,7 +109,7 @@ void flush_written_regions(struct cpu_mapping *mapping)
 					continue;
 				}
 
-				struct gpu_mapping *gpu_mapping = gpu_mapping_find(gpu_addr);
+				struct gpu_mapping *gpu_mapping = gpu_mapping_find(gpu_addr, dev);
 				if (!gpu_mapping)
 				{
 					cur = cur->next;
@@ -135,13 +139,9 @@ void flush_written_regions(struct cpu_mapping *mapping)
 		unsigned char *data = mapping->data;
 		uint32_t addr = cur->start;
 
-		uint64_t gpu_addr = 0;
-		if (print_gpu_addresses)
-		{
-			gpu_addr = cpu_mapping_to_gpu_addr(mapping, cur->start);
-			if (gpu_addr)
-				gpu_addr -= cur->start;
-		}
+		uint64_t gpu_addr = cpu_mapping_to_gpu_addr(mapping, cur->start);
+		if (gpu_addr)
+			gpu_addr -= cur->start;
 
 		int left = cur->end - addr;
 		if (!mapping->ib.is && !mapping->user.is &&
@@ -195,12 +195,16 @@ void flush_written_regions(struct cpu_mapping *mapping)
 		if (mapping->ib.is)
 		{
 			if (addr >= mapping->ib.offset && addr < mapping->length)
+			{
 				ib_decode_start(&mapping->ib.state);
+				mapping->ib.state.pstate.fifo = nvrm_get_fifo(mapping->object, gpu_addr + addr);
+			}
 		}
 		else if (mapping->user.is)
 		{
 			if (0)
 				user_decode_start(&mapping->user.state);
+			mapping->user.state.pstate.fifo = nvrm_get_fifo(mapping->object, gpu_addr + addr);
 		}
 
 		while (addr < cur->end)

demmt/config.c
@@ -58,7 +58,6 @@ int pager_enabled = 1;
 int dump_object_tree_on_create_destroy = 1;
 
 int chipset;
-int ib_supported;
 int indent_logs = 0;
 int is_nouveau = 0;
 int dump_memory_writes = 1;
@@ -550,10 +549,6 @@ char *read_opts(int argc, char *argv[])
 				break;
 		}
 	}
-
-	if (chipset == 0)
-		usage();
-	ib_supported = chipset >= 0x80 || chipset == 0x50;
 
 	return filename;
 }

demmt/config.h
@@ -6,7 +6,6 @@
 extern const struct envy_colors *colors;
 
 extern int chipset;
-extern int ib_supported;
 extern int is_nouveau;
 extern int dump_raw_ioctl_data;
 extern int dump_decoded_ioctl_data;

demmt/demmt.h
@@ -10,7 +10,7 @@
 extern struct rnndomain *domain;
 extern struct rnndb *rnndb;
 extern struct rnndb *rnndb_nvrm_object;
-extern struct rnndeccontext *g80_texture_ctx;
+extern struct rnndb *rnndb_g80_texture;
 extern struct rnndeccontext *gf100_shaders_ctx;
 extern struct rnndomain *tsc_domain;
 extern struct rnndomain *tic_domain;

demmt/drm.c
@@ -33,6 +33,8 @@
 #include "config.h"
 #include "drm.h"
 #include "log.h"
+#include "nvrm.h"
+#include "nvrm_object.xml.h"
 #include "pushbuf.h"
 #include "util.h"
 
@@ -274,6 +276,8 @@ int demmt_drm_ioctl_pre(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size, str
 
 int demmt_drm_ioctl_post(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size, struct mmt_buf *buf, void *state)
 {
+	static int nouveau_chipset; // hack
+
 	void *ioctl_data = buf->data;
 
 	int i;
@@ -286,6 +290,8 @@ int demmt_drm_ioctl_post(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size, st
 					colors->rname, colors->reset, colors->eval,
 					data->param < ARRAY_SIZE(nouveau_param_names) ? nouveau_param_names[data->param] : "???",
 					colors->reset, data->param, colors->num, data->value, colors->reset);
+		if (data->param == NOUVEAU_GETPARAM_CHIPSET_ID)
+			nouveau_chipset = data->value;
 	}
 	else if (nr == DRM_COMMAND_BASE + DRM_NOUVEAU_SETPARAM)
 	{
@@ -312,6 +318,13 @@ int demmt_drm_ioctl_post(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size, st
 					mmt_log_cont(" subchan[%d]=<h:0x%0x, c:0x%0x>", i, data->subchan[i].handle, data->subchan[i].grclass);
 			mmt_log_cont("%s\n", "");
 		}
+		// hack, fake device
+		struct gpu_object *dev = gpu_object_add(fd, data->channel, data->channel, data->channel, NVRM_DEVICE_0);
+		nvrm_device_set_chipset(dev, nouveau_chipset);
+
+		// hack, fake fifo
+		struct gpu_object *fifo = gpu_object_add(fd, data->channel, data->channel, 0xf1f0eeee, NVRM_FIFO_IB_G80);
+		get_fifo_state(fifo);
 	}
 	else if (nr == DRM_COMMAND_BASE + DRM_NOUVEAU_CHANNEL_FREE)
 	{
@@ -320,6 +333,8 @@ int demmt_drm_ioctl_post(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size, st
 		if (0 && dump_decoded_ioctl_data) // -> pre
 			mmt_log("%sDRM_NOUVEAU_CHANNEL_FREE%s, channel: %d\n", colors->rname,
 					colors->reset, data->channel);
+
+		gpu_object_destroy(gpu_object_find(data->channel, data->channel));
 	}
 	else if (nr == DRM_COMMAND_BASE + DRM_NOUVEAU_GROBJ_ALLOC)
 	{
@@ -331,8 +346,8 @@ int demmt_drm_ioctl_post(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size, st
 					data->handle, colors->reset, colors->eval, data->class,
 					colors->reset);
 
-		pushbuf_add_object(data->class, data->class); // yes, class x2
-		gpu_object_add(fd, 0, 0, data->handle, data->class);
+		struct gpu_object *gpu_obj = gpu_object_add(fd, data->channel, 0xf1f0eeee, data->handle, data->class);
+		pushbuf_add_object(data->class, data->class, gpu_obj); // yes, class x2
 	}
 	else if (nr == DRM_COMMAND_BASE + DRM_NOUVEAU_NOTIFIEROBJ_ALLOC)
 	{
@@ -363,7 +378,7 @@ int demmt_drm_ioctl_post(uint32_t fd, uint8_t dir, uint8_t nr, uint16_t size, st
 			dump_drm_nouveau_gem_new(g);
 		}
 
-		struct gpu_object *obj = gpu_object_add(fd, 0, 0, g->info.handle, 0);
+		struct gpu_object *obj = gpu_object_add(fd, g->channel_hint, g->channel_hint, g->info.handle, 0);
 		obj->length = g->info.size;
 		obj->data = realloc(obj->data, obj->length);
 		memset(obj->data, 0, obj->length);
@@ -516,12 +531,14 @@ void demmt_nouveau_gem_pushbuf_data(struct mmt_nouveau_pushbuf_data *data, void 
 
 	struct pushbuf_decode_state pstate;
 	pushbuf_decode_start(&pstate);
+	pstate.fifo = gpu_object_find(0, 0xf1f0eeee); // hack
+	struct gpu_object *dev = nvrm_get_device(pstate.fifo);
 
 	for (i = 0; i < nr_push; ++i)
 	{
 		uint64_t gpu_start = buffers[push[i].bo_index].presumed.offset;
 
-		struct gpu_mapping *gmapping = gpu_mapping_find(gpu_start);
+		struct gpu_mapping *gmapping = gpu_mapping_find(gpu_start, dev);
 		if (gmapping)
 			pushbuf_print(&pstate, gmapping, gpu_start + push[i].offset, push[i].length / 4);
 		else

commit fab682bdd57e16055789906bdc87d6c4eb9aa5f8

demmt: fix handle guessing
Broken since f5f2659cf22c50458a37b4a0337620ac21b658a2
"demmt: multi-device/context support and automatic chipset detection"

demmt/pushbuf.c
@@ -198,7 +198,8 @@ static struct obj *get_object(uint32_t handle, struct gpu_object *gpu_obj)
 		else
 		{
 			mmt_error("Guessing handle 0x%08x, driver forgot to call NVRM_MTHD_FIFO_IB_OBJECT_INFO?\n", handle);
-			pushbuf_add_object(handle, handle & 0xffff, gpu_obj);
+			struct gpu_object *gpu_obj2 = gpu_object_add(gpu_obj->fd, gpu_obj->cid, gpu_obj->handle, handle, handle & 0xffff);
+			pushbuf_add_object(handle, handle & 0xffff, gpu_obj2);
 		}
 
 		return get_object(handle, gpu_obj);