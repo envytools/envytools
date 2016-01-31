7d76222ff2a1c21bf786820239b478c11935a92f
Marcin Kościelnicki <koriakin@0x04.net>
2016-01-31 23:40:38 +0100

    hwtest/g80_int: Add icmp test.

--- hwtest/g80_int.c
+++ hwtest/g80_int.c
@@ -213,19 +213,36 @@ static int fp_check_data(struct hwtest_ctx *ctx, uint32_t op1, uint32_t op2, con
 		}
 		int sop;
 		switch (op) {
-			case 0x20: /* add */
-			case 0x30: /* add */
+			case 0x20: /* iadd */
+			case 0x30: /* iadd */
 				sop = (op1 >> 22 & 1) | (op1 >> 27 & 2);
 				exp = g80_add(op2 >> 26 & 1, sop, op2 >> 27 & 1, s1, s3, scc, &ecc);
 				if (!(op2 >> 26 & 1))
 					real &= 0xffff;
 				break;
-			case 0x28: /* add */
-			case 0x38: /* add */
+			case 0x28: /* iadd */
+			case 0x38: /* iadd */
 				sop = (op1 >> 22 & 1) | (op1 >> 27 & 2);
 				exp = g80_add(op1 >> 15 & 1, sop, op1 >> 8 & 1, s1, s2, scc, 0);
 				if (!(op1 >> 15 & 1))
 					real &= 0xffff;
+				break;
+			case 0x33: /* icmp */
+				if (!(op2 >> 26 & 1)) {
+					s1 <<= 16;
+					s2 <<= 16;
+				}
+				if (op2 >> 27 & 1) {
+					s1 ^= 1 << 31;
+					s2 ^= 1 << 31;
+				}
+				exp = (s1 < s2 ? 0 : s1 == s2 ? 1 : 2);
+				exp = (op2 >> (14 + exp) & 1) ? -1 : 0;
+				ecc = exp ? 2 : 1;
+				if (!(op2 >> 26 & 1)) {
+					real &= 0xffff;
+					exp &= 0xffff;
+				}
 				break;
 			default:
 				abort();
@@ -264,7 +281,7 @@ static void fp_gen(struct hwtest_ctx *ctx, uint32_t *src1, uint32_t *src2, uint3
 	}
 }
 
-static int test_add(struct hwtest_ctx *ctx) {
+static int test_iadd(struct hwtest_ctx *ctx) {
 	int i;
 	for (i = 0; i < 100000; i++) {
 		uint32_t op1 = 0x20000001 | (jrand48(ctx->rand48) & 0x1e7f0000);
@@ -292,7 +309,33 @@ static int test_add(struct hwtest_ctx *ctx) {
 	return HWTEST_RES_PASS;
 }
 
-static int test_add_s(struct hwtest_ctx *ctx) {
+static int test_icmp(struct hwtest_ctx *ctx) {
+	int i;
+	for (i = 0; i < 100000; i++) {
+		uint32_t op1 = 0x30000001 | (jrand48(ctx->rand48) & 0x0e000000);
+		uint32_t op2 = 0x600007d0 | (jrand48(ctx->rand48) & 0x1fdfc004);
+		uint32_t xtra = jrand48(ctx->rand48);
+		if (op2 >> 26 & 1) {
+			/* 32-bit */
+			op1 |= 0x5081c;
+		} else {
+			/* 16-bit */
+			op1 |= 0xa1038;
+		}
+		if (fp_prep_grid(ctx, xtra))
+			return HWTEST_RES_FAIL;
+		if (fp_prep_code(ctx, op1, op2))
+				return HWTEST_RES_FAIL;
+		uint32_t src1[0x200], src2[0x200], src3[0x200];
+		uint8_t cc[0x200];
+		fp_gen(ctx, src1, src2, src3, cc);
+		if (fp_test(ctx, op1, op2, src1, src2, src3, cc, xtra))
+			return HWTEST_RES_FAIL;
+	}
+	return HWTEST_RES_PASS;
+}
+
+static int test_iadd_s(struct hwtest_ctx *ctx) {
 	int i;
 	for (i = 0; i < 100000; i++) {
 		uint32_t op1 = 0x20000000 | (jrand48(ctx->rand48) & 0x1e408100);
@@ -318,7 +361,7 @@ static int test_add_s(struct hwtest_ctx *ctx) {
 	return HWTEST_RES_PASS;
 }
 
-static int test_add_i(struct hwtest_ctx *ctx) {
+static int test_iadd_i(struct hwtest_ctx *ctx) {
 	int i;
 	for (i = 0; i < 100000; i++) {
 		uint32_t op1 = 0x20000001 | (jrand48(ctx->rand48) & 0x1e7f8100);
@@ -345,7 +388,8 @@ static int test_add_i(struct hwtest_ctx *ctx) {
 }
 
 HWTEST_DEF_GROUP(g80_int,
-	HWTEST_TEST(test_add, 0),
-	HWTEST_TEST(test_add_s, 0),
-	HWTEST_TEST(test_add_i, 0),
+	HWTEST_TEST(test_iadd, 0),
+	HWTEST_TEST(test_icmp, 0),
+	HWTEST_TEST(test_iadd_s, 0),
+	HWTEST_TEST(test_iadd_i, 0),
 )
