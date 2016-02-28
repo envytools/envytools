011c342c798c993ede250027b59f935b6f50c3ae
Karol Herbst <git@karolherbst.de>
2016-02-28 09:37:16 +0100

    nvbios: show both max voltage entries
    for whatever reasons they behave the same, so for now they are unks

--- nvbios/nvbios.c
+++ nvbios/nvbios.c
@@ -1647,7 +1647,8 @@ int main(int argc, char **argv) {
 			}
 			break;
 		case 0x20:
-			printf("boost_voltage_range_entry: %i\n\n", bios->data[start - header_length + 0x7]);
+			printf("unk7 max entry: %i\n", bios->data[start - header_length + 0x7]);
+			printf("unk8 max entry: %i\n\n", bios->data[start - header_length + 0x8]);
 			for (i=0; i < entry_count; i++) {
 				printf ("-- ID = %u, mode: %i link: %hhx, voltage_min = %u, voltage_max = %u [µV]"
 						" c0 %i c1 %i c2 %i c3 %i c4 %i c5 %i--\n",
