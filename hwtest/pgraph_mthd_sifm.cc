0c5aa9d421292175f8b48776046d4ec4e8590b4e
Marcin Kościelnicki <koriakin@0x04.net>
2016-12-04 12:21:36 +0100

    hwtest/pgraph: Convert remaining nv3 sifm mthd tests to class-based.

--- hwtest/pgraph_mthd_sifm.cc
+++ hwtest/pgraph_mthd_sifm.cc
@@ -30,6 +30,156 @@ namespace pgraph {
 
 namespace {
 
+class MthdSifmVtxTest : public MthdTest {
+	int repeats() override { return 10000; };
+	void choose_mthd() override {
+		cls = 0x0e;
+		mthd = 0x310;
+		if (!(rnd() & 3))
+			val &= 0x001f000f;
+	}
+	void emulate_mthd() override {
+		insrt(exp.valid[0], 0, 1, 1);
+		insrt(exp.valid[0], 12, 1, 1);
+		exp.vtx_x[0] = extrs(val, 0, 16);
+		exp.vtx_y[4] = extrs(val, 16, 16);
+		insrt(exp.valid[0], 19, 1, 0);
+		insrt(exp.xy_misc_0, 28, 4, 1);
+		insrt(exp.xy_misc_1[0], 0, 1, 0);
+		insrt(exp.xy_misc_1[1], 0, 1, 1);
+		int xcstat = nv03_pgraph_clip_status(&exp, exp.vtx_x[0], 0, false);
+		int ycstat = nv03_pgraph_clip_status(&exp, exp.vtx_y[4], 1, false);
+		insrt(exp.xy_clip[0][0], 0, 4, xcstat);
+		insrt(exp.xy_clip[1][0], 0, 4, ycstat);
+		insrt(exp.xy_misc_3, 8, 1, 0);
+		insrt(exp.xy_misc_4[0], 0, 1, 0);
+		insrt(exp.xy_misc_4[0], 4, 1, 0);
+		insrt(exp.xy_misc_4[1], 0, 1, 0);
+		insrt(exp.xy_misc_4[1], 4, 1, 0);
+	}
+public:
+	MthdSifmVtxTest(TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
+};
+
+class MthdSifmRectTest : public MthdTest {
+	int repeats() override { return 10000; };
+	void choose_mthd() override {
+		cls = 0x0e;
+		mthd = 0x314;
+		if (!(rnd() & 3))
+			val &= 0x001f000f;
+	}
+	void emulate_mthd() override {
+		insrt(exp.valid[0], 1, 1, 1);
+		insrt(exp.valid[0], 9, 1, 1);
+		nv03_pgraph_vtx_add(&exp, 0, 1, exp.vtx_x[0], extr(val, 0, 16), 0, false);
+		nv03_pgraph_vtx_add(&exp, 1, 1, 0, extr(val, 16, 16), 0, false);
+		int vtxid = extr(exp.xy_misc_0, 28, 4);
+		int nvtxid = (vtxid + 1) & 1;
+		insrt(exp.xy_misc_0, 28, 4, nvtxid);
+		nv03_pgraph_vtx_cmp(&exp, 0, 8, true);
+		nv03_pgraph_vtx_cmp(&exp, 1, 8, true);
+	}
+public:
+	MthdSifmRectTest(TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
+};
+
+class MthdSifmDuDxTest : public MthdTest {
+	int repeats() override { return 10000; };
+	void choose_mthd() override {
+		cls = 0x0e;
+		mthd = 0x318;
+		if (!(rnd() & 3))
+			val = sext(val, 21);
+	}
+	void emulate_mthd() override {
+		int sv = val >> 17;
+		if (sv >= 0x4000) {
+			if (sv >= 0x7ff8)
+				sv &= 0xf;
+			else
+				sv = 9;
+		} else {
+			if (sv > 7)
+				sv = 7;
+		}
+		insrt(exp.valid[0], 4, 1, 1);
+		exp.misc32[0] = val;
+		insrt(exp.xy_misc_1[0], 24, 4, sv);
+	}
+public:
+	MthdSifmDuDxTest(TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
+};
+
+class MthdSifmDvDyTest : public MthdTest {
+	int repeats() override { return 10000; };
+	void choose_mthd() override {
+		cls = 0x0e;
+		mthd = 0x31c;
+		if (!(rnd() & 3))
+			val = sext(val, 21);
+	}
+	void emulate_mthd() override {
+		int sv = val >> 17;
+		if (sv >= 0x4000) {
+			if (sv >= 0x7ff8)
+				sv &= 0xf;
+			else
+				sv = 9;
+		} else {
+			if (sv > 7)
+				sv = 7;
+		}
+		insrt(exp.valid[0], 5, 1, 1);
+		exp.vtx_y[3] = val;
+		insrt(exp.xy_misc_1[1], 24, 4, sv);
+	}
+public:
+	MthdSifmDvDyTest(TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
+};
+
+class MthdSifmSrcSizeTest : public MthdTest {
+	bool yuv420;
+	int repeats() override { return 10000; };
+	void choose_mthd() override {
+		yuv420 = rnd() & 1;
+		cls = 0x0e;
+		mthd = 0x400;
+		if (rnd() & 1) {
+			val &= 0x0fff0fff;
+			val ^= 1 << (rnd() & 0x1f);
+		}
+		if (!(rnd() & 3)) {
+			val &= ~0x7f0;
+			val |= 0x400;
+		}
+		if (!(rnd() & 3)) {
+			val &= ~0x7f00000;
+			val |= 0x4000000;
+		}
+	}
+	int gen_nv3_fmt() override {
+		return yuv420 ? 7 : rnd() % 7;
+	}
+	bool is_valid_val() override {
+		return (extr(val, 0, 16) <= 0x400 && extr(val, 16, 16) <= 0x400);
+	}
+	void emulate_mthd() override {
+		exp.misc24[0] = extr(val, 0, 16);
+		insrt(exp.misc24[1], 0, 11, extr(val, 0, 11));
+		insrt(exp.misc24[1], 11, 11, extr(val, 16, 11));
+		if (yuv420) {
+			insrt(exp.valid[0], 9, 1, 0);
+			insrt(exp.valid[0], 13, 1, 1);
+		} else {
+			insrt(exp.valid[0], 9, 1, 1);
+			insrt(exp.valid[0], 13, 1, 0);
+		}
+	}
+public:
+	MthdSifmSrcSizeTest(TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
+};
+
 class MthdSifmOffsetTest : public MthdTest {
 	int idx;
 	bool yuv420;
@@ -58,9 +208,8 @@ class MthdSifmOffsetTest : public MthdTest {
 		}
 	}
 public:
-	MthdSifmOffsetTest(hwtest::TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
+	MthdSifmOffsetTest(TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
 };
-
 
 class MthdSifmPitchTest : public MthdTest {
 	bool yuv420;
@@ -91,7 +240,7 @@ class MthdSifmPitchTest : public MthdTest {
 		}
 	}
 public:
-	MthdSifmPitchTest(hwtest::TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
+	MthdSifmPitchTest(TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
 };
 
 class MthdSifmInvalidTest : public MthdTest {
@@ -108,7 +257,7 @@ class MthdSifmInvalidTest : public MthdTest {
 		exp.fifo_enable = 0;
 	}
 public:
-	MthdSifmInvalidTest(hwtest::TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
+	MthdSifmInvalidTest(TestOptions &opt, uint32_t seed) : MthdTest(opt, seed) {}
 };
 
 }
@@ -119,6 +268,11 @@ bool PGraphMthdSifmTests::supported() {
 
 Test::Subtests PGraphMthdSifmTests::subtests() {
 	return {
+		{"vtx", new MthdSifmVtxTest(opt, rnd())},
+		{"rect", new MthdSifmRectTest(opt, rnd())},
+		{"du_dx", new MthdSifmDuDxTest(opt, rnd())},
+		{"dv_dy", new MthdSifmDvDyTest(opt, rnd())},
+		{"src_size", new MthdSifmSrcSizeTest(opt, rnd())},
 		{"offset", new MthdSifmOffsetTest(opt, rnd())},
 		{"pitch", new MthdSifmPitchTest(opt, rnd())},
 		{"invalid", new MthdSifmInvalidTest(opt, rnd())},
