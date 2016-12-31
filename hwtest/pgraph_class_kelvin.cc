aacb461a80368532787dad1ff9378bb90a8a46f3
Marcin Kościelnicki <koriakin@0x04.net>
2016-12-31 02:00:17 +0100

    hwtest/pgraph: Bring back trapbits to EmuCelsius.

--- hwtest/pgraph_class_kelvin.cc
+++ hwtest/pgraph_class_kelvin.cc
@@ -33,24 +33,24 @@ namespace pgraph {
 std::vector<SingleMthdTest *> EmuCelsius::mthds() {
 	std::vector<SingleMthdTest *> res = {
 		new MthdNop(opt, rnd(), "nop", -1, cls, 0x100),
-		new MthdNotify(opt, rnd(), "notify", -1, cls, 0x104),
-		new MthdWarning(opt, rnd(), "warning", -1, cls, 0x108),
+		new MthdNotify(opt, rnd(), "notify", 0, cls, 0x104),
+		new MthdWarning(opt, rnd(), "warning", 2, cls, 0x108),
 		new MthdState(opt, rnd(), "state", -1, cls, 0x10c),
-		new MthdSync(opt, rnd(), "sync", -1, cls, 0x110),
+		new MthdSync(opt, rnd(), "sync", 1, cls, 0x110),
 		new MthdPmTrigger(opt, rnd(), "pm_trigger", -1, cls, 0x140),
-		new MthdDmaNotify(opt, rnd(), "dma_notify", -1, cls, 0x180),
-		new UntestedMthd(opt, rnd(), "dma_tex_a", -1, cls, 0x184), // XXX
-		new UntestedMthd(opt, rnd(), "dma_tex_b", -1, cls, 0x188), // XXX
-		new UntestedMthd(opt, rnd(), "dma_vtx", -1, cls, 0x18c), // XXX
-		new UntestedMthd(opt, rnd(), "dma_state", -1, cls, 0x190), // XXX
-		new MthdDmaSurf(opt, rnd(), "dma_surf_color", -1, cls, 0x194, 2, SURF_NV10),
-		new MthdDmaSurf(opt, rnd(), "dma_surf_zeta", -1, cls, 0x198, 3, SURF_NV10),
-		new UntestedMthd(opt, rnd(), "clip_h", -1, cls, 0x200), // XXX
-		new UntestedMthd(opt, rnd(), "clip_v", -1, cls, 0x204), // XXX
-		new MthdSurf3DFormat(opt, rnd(), "surf_format", -1, cls, 0x208, true),
-		new MthdSurfPitch2(opt, rnd(), "surf_pitch_2", -1, cls, 0x20c, 2, 3, SURF_NV10),
-		new MthdSurfOffset(opt, rnd(), "color_offset", -1, cls, 0x210, 2, SURF_NV10),
-		new MthdSurfOffset(opt, rnd(), "zeta_offset", -1, cls, 0x214, 3, SURF_NV10),
+		new MthdDmaNotify(opt, rnd(), "dma_notify", 3, cls, 0x180),
+		new UntestedMthd(opt, rnd(), "dma_tex_a", 4, cls, 0x184), // XXX
+		new UntestedMthd(opt, rnd(), "dma_tex_b", 5, cls, 0x188), // XXX
+		new UntestedMthd(opt, rnd(), "dma_vtx", 6, cls, 0x18c), // XXX
+		new UntestedMthd(opt, rnd(), "dma_state", 7, cls, 0x190), // XXX
+		new MthdDmaSurf(opt, rnd(), "dma_surf_color", 8, cls, 0x194, 2, SURF_NV10),
+		new MthdDmaSurf(opt, rnd(), "dma_surf_zeta", 9, cls, 0x198, 3, SURF_NV10),
+		new UntestedMthd(opt, rnd(), "clip_h", 10, cls, 0x200), // XXX
+		new UntestedMthd(opt, rnd(), "clip_v", 10, cls, 0x204), // XXX
+		new MthdSurf3DFormat(opt, rnd(), "surf_format", 11, cls, 0x208, true),
+		new MthdSurfPitch2(opt, rnd(), "surf_pitch_2", 12, cls, 0x20c, 2, 3, SURF_NV10),
+		new MthdSurfOffset(opt, rnd(), "color_offset", 13, cls, 0x210, 2, SURF_NV10),
+		new MthdSurfOffset(opt, rnd(), "zeta_offset", 14, cls, 0x214, 3, SURF_NV10),
 		new UntestedMthd(opt, rnd(), "meh", -1, cls, 0x218, 2), // XXX
 		new UntestedMthd(opt, rnd(), "meh", -1, cls, 0x220, 8), // XXX
 		new UntestedMthd(opt, rnd(), "meh", -1, cls, 0x240, 0x10), // XXX
