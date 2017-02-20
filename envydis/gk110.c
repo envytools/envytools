dadeaf4c15a39de6649bfff803d8203a7536d566
Boyan Ding <boyan.j.ding@gmail.com>
2017-02-20 23:38:53 +0800

    gk110: Add missing flags for addop3b LIMM form
    Results from nvdisasm:
    00000001 41000000:	@P0 IADD32I.X R0, R0, 0x0
    00000001 40800000:	@P0 IADD32I R0.CC, R0, 0x0

--- envydis/gk110.c
+++ envydis/gk110.c
@@ -895,6 +895,7 @@ F1(acout32, 0x32, CC)
 F1(acout37, 0x37, CC)
 F1(acin2e, 0x2e, CC)
 F1(acin34, 0x34, CC)
+F1(acin38, 0x38, CC)
 F1(acin3c, 0x3c, CC)
 
 F1(shiftamt, 0x2c, N("shiftamt"))
@@ -1585,7 +1586,7 @@ static struct insn tabm[] = {
 };
 
 static struct insn tabi[] = {
-	{ 0x4000000000000001ull, 0xf580000000000003ull, T(addop3b), T(sat39), N("b32"), DST, SRC1, LIMM },
+	{ 0x4000000000000001ull, 0xf400000000000003ull, T(addop3b), T(sat39), N("b32"), DST, T(acout37), SRC1, LIMM , T(acin38) },
 	{ 0x8000000000000001ull, 0xf180000000000003ull, N("set"), T(ftz3a), N("b32"), DST, T(acout32), T(setit), N("f32"), T(neg2e), T(abs39), SRC1, T(neg3b), FIMM, T(setlop3) }, // XXX: find f32 dst
 	{ 0x0040000000000001ull, 0x37c0000000000003ull, N("popc"), N("b32"), DST, T(not2a), SRC1, T(i3bi2) },
 	{ 0x0080000000000001ull, 0x37c0000000000003ull, T(addop), T(sat35), N("b32"), DST, T(acout32), SRC1, T(i3bi2) },
