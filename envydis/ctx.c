a8b5854acadef5132373cc3afd07395a3b23c80d
Marcin Kościelnicki <koriakin@0x04.net>
2018-06-05 13:48:12 +0200

    envydis/ctx: Add a lot of things REd in the meantime.

--- envydis/ctx.c
+++ envydis/ctx.c
@@ -46,8 +46,7 @@
  * 0x400308: Next opcode to execute, or the exit opcode if program exitted.
  * 0x40030c: RO alias of PC for some reason
  * 0x400310: PC. Readable and writable. In units of single insns.
- * 0x40031c: the scratch register, written by opcode 2, holding params to
- *           various stuff.
+ * 0x40031c: register $ra (20-bit), holding parameters to various stuff.
  * 0x400320: WO, CMD register. Writing here launches a cmd, see below.
  * 0x400324: code upload index, WO. selects address to write in ctxprog code,
  *           counted in whole insns.
@@ -57,7 +56,8 @@
  * 0x400330: next channel RAMIN address, shifted right by 12 bits. bit 31 == valid
  * 0x400334: $r register, offset in RAMIN context to read/write with opcode 1.
  *           in units of 32-bit words.
- * 0x400338: Some register, set with opcode 3. Used on NVAx with values 0-9 to
+ * 0x400338: register $rb (20-bit), holding parameters to various stuff.
+ *           Used on NVAx with values 0-9 to
  *           select TP number to read/write with G[0x8000]. Maybe generic
  *           PGRAPH offset? Also set to 0x8ffff in one place for some reason.
  * 0x40033c: Some other register. Set with opcode 0x600007. Related to and
@@ -134,8 +134,10 @@ static struct rbitfield ctargoff = { 8, 9 };
 
 /* Register fields */
 
-static struct reg sr_r = { 0, "sr" };
-#define SR atomreg, &sr_r
+static struct reg ra_r = { 0, "ra" };
+static struct reg rb_r = { 0, "rb" };
+#define RA atomreg, &ra_r
+#define RB atomreg, &rb_r
 
 /*
  * Misc number fields
@@ -143,23 +145,27 @@ static struct reg sr_r = { 0, "sr" };
  * Used for plain numerical arguments.
  */
 
-static struct bitfield unitoff = { 0, 5 };
+static struct bitfield scratchbitoff = { 0, 5 };
 static struct bitfield flagoff = { 0, 7 };
 static struct bitfield gsize4off = { 14, 6 };
 static struct bitfield gsize5off = { 16, 4 };
 static struct bitfield immoff = { 0, 20 };
-static struct bitfield dis0off = { 0, 16 };
-static struct bitfield dis1off = { { 0, 16 }, BF_UNSIGNED, 16 };
-static struct bitfield seekpoff = { 8, 11 };
+static struct bitfield rmaskoff = { 0, 16 };
+static struct bitfield ridxoff = { 16, 4 };
+static struct bitfield xstart4off = { 3, 7 };
+static struct bitfield xstart5off = { 8, 11 };
+static struct bitfield xmaskoff = { 0, 8 };
 static struct bitfield cmdoff = { 0, 5 };
-#define UNIT atomimm, &unitoff
+#define SCRATCHBIT atomimm, &scratchbitoff
 #define FLAG atomimm, &flagoff
 #define GSIZE4 atomimm, &gsize4off
 #define GSIZE5 atomimm, &gsize5off
 #define IMM atomimm, &immoff
-#define DIS0 atomimm, &dis0off
-#define DIS1 atomimm, &dis1off
-#define SEEKP atomimm, &seekpoff
+#define RMASK atomimm, &rmaskoff
+#define RIDX atomimm, &ridxoff
+#define XSTART4 atomimm, &xstart4off
+#define XSTART5 atomimm, &xstart5off
+#define XMASK atomimm, &xmaskoff
 #define CMD atomimm, &cmdoff
 
 /*
@@ -175,33 +181,37 @@ static struct mem pgmem5_m = { "G", 0, 0, &pgmem5_imm };
 #define PGRAPH4 atommem, &pgmem4_m
 #define PGRAPH5 atommem, &pgmem5_m
 
-F1(p0, 0, N("a0"))
-F1(p1, 1, N("a1"))
-F1(p2, 2, N("a2"))
-F1(p3, 3, N("a3"))
-F1(p4, 4, N("a4"))
-F1(p5, 5, N("a5"))
-F1(p6, 6, N("a6"))
-F1(p7, 7, N("a7"))
-
-static struct insn tabarea[] = {
-	{ 0, 0, T(p0), T(p1), T(p2), T(p3), T(p4), T(p5), T(p6), T(p7) },
-};
-
 
 static struct insn tabrpred[] = {
-	{ 0x00, 0x7f, N("dir") }, // the direction flag; 0: read from memory, 1: write to memory
-	{ 0x1c, 0x7f, N("pm0") },
-	{ 0x1d, 0x7f, N("pm1") },
-	{ 0x1e, 0x7f, N("pm2") },
-	{ 0x1f, 0x7f, N("pm3") },
-	{ 0x4a, 0x7f, N("newctxdone"), .fmask = F_G80 },	// newctx CMD finished with loading new address... or something like that, it seems to be turned back off *very shortly* after the newctx CMD. only check it with a wait right after newctx. weird.
-	{ 0x4b, 0x7f, N("xferbusy"), .fmask = F_G80 },	// RAMIN xfer in progress
-	{ 0x4c, 0x7f, N("delaydone"), .fmask = F_G80 },
-	{ 0x4d, 0x7f, .fmask = F_G80 },	// always
-	{ 0x60, 0x60, N("unit"), UNIT, .fmask = F_G80 }, // if given unit present
+	{ 0x00, 0x7f, N("dirsave") }, // the direction flag; 0: read from memory, 1: write to memory
+	{ 0x03, 0x7f, N("ctxfreeze"), .fmask = F_G80 }, // disallows all modification of context, so that S/R can proceed
+	{ 0x05, 0x7f, N("swsavereq") }, // SW defined
+	{ 0x06, 0x7f, N("swrestorereq") }, // SW defined
+	{ 0x09, 0x7f, N("swxferreq") }, // SW defined
+	{ 0x1c, 0x7f, N("pm0"), .fmask = F_G80 },
+	{ 0x1d, 0x7f, N("pm1"), .fmask = F_G80 },
+	{ 0x1e, 0x7f, N("pm2"), .fmask = F_G80 },
+	{ 0x1f, 0x7f, N("pm3"), .fmask = F_G80 },
+	// NV40 hardware RO flags
+	{ 0x60, 0x7f, N("idle"), .fmask = F_NV40 },
+	{ 0x64, 0x7f, N("hwsavereq"), .fmask = F_NV40 },
+	{ 0x65, 0x7f, N("hwrestorereq"), .fmask = F_NV40 },
+	{ 0x66, 0x7f, N("state3d"), .fmask = F_NV40 },	// wired straight to the CTX_USER bit.
 	{ 0x68, 0x7f, .fmask = F_NV40 },	// always
-	{ 0x00, 0x00, N("flag"), FLAG },
+	{ 0x69, 0x7f, N("timeridle"), .fmask = F_NV40 },
+	// G80 hardware RO flags
+	{ 0x40, 0x7f, N("idle"), .fmask = F_G80 },
+	{ 0x44, 0x7f, N("hwsavereq"), .fmask = F_G80 },
+	{ 0x45, 0x7f, N("hwrestorereq"), .fmask = F_G80 },
+	{ 0x4a, 0x7f, N("binddmaack"), .fmask = F_G80 },	// bind ctx dma CMD finished with loading new address... or something like that, it seems to be turned back off *very shortly* after the CMD. only check it with a wait right after cmd. weird.
+	{ 0x4b, 0x7f, N("xferbusy"), .fmask = F_G80 },	// RAMIN xfer in progress
+	{ 0x4c, 0x7f, N("timeridle"), .fmask = F_G80 },
+	{ 0x4d, 0x7f, .fmask = F_G80 },	// always
+	{ 0x4f, 0x7f, N("intrpending"), .fmask = F_G80 },
+	// G80 scratch reg
+	{ 0x60, 0x60, N("scratch"), SCRATCHBIT, .fmask = F_G80 },
+	// unknown
+	{ 0x00, 0x00, N("flag"), FLAG, OOPS },
 };
 
 static struct insn tabpred[] = {
@@ -210,48 +220,67 @@ static struct insn tabpred[] = {
 	{ 0, 0, OOPS },
 };
 
-static struct insn tabcmd5[] = {
-	{ 0x04, 0x1f, C("NEWCTX"), .fmask = F_G80 },		// fetches grctx DMA object from channel object in 784
-	{ 0x05, 0x1f, C("NEXT_TO_SWAP"), .fmask = F_G80 },	// copies 330 [new channel] to 784 [channel used for ctx RAM access]
-	{ 0x06, 0x1f, C("SET_CONTEXT_POINTER"), .fmask = F_G80 },	// copies scratch to 334
-	{ 0x07, 0x1f, C("SET_XFER_POINTER"), .fmask = F_G80 },	// copies scratch to 33c, anding it with 0xffff8
-	{ 0x08, 0x1f, C("DELAY"), .fmask = F_G80 },		// sleeps scratch cycles, then briefly lights up delaydone
-	{ 0x09, 0x1f, C("ENABLE"), .fmask = F_G80 },		// resets 0x40 to 0
-	{ 0x0c, 0x1f, C("END"), .fmask = F_G80 },			// halts program execution, resets PC to 0
-	{ 0x0d, 0x1f, C("NEXT_TO_CURRENT"), .fmask = F_G80 },	// movs new channel RAMIN address to current channel RAMIN address, basically where the real switch happens
-	{ 0, 0, CMD },
+static struct insn tabcmd4[] = {
+	{ 0x07, 0x1f, C("BIND_CHANNEL") },	// copies 330 [new channel] to 784 [channel used for ctx RAM access]
+	{ 0x09, 0x1f, C("FINISH_CHSW") },	// movs new channel RAMIN address to current channel RAMIN address, making PFIFO happy
+	{ 0x0a, 0x1f, C("SET_CTX_POINTER") },	// copies scratch to 334
+	{ 0x0b, 0x1f, C("START_TIMER") },	// sets the timer to count down $ra cycles
+	{ 0x0c, 0x1f, C("CLEAR_RESET") },	// undoes all reset commands
+	{ 0x0d, 0x1f, C("HALT") },		// halt execution *without* resetting PC to 0
+	{ 0x0e, 0x1f, C("END") },		// halts program execution, resets PC to 0
+	{ 0x0f, 0x1f, C("INTR") },		// trigger context switch interrupt and halt
+	{ 0, 0, OOPS, CMD },
 };
 
-static struct insn tabcmd4[] = {
-	{ 0x07, 0x1f, C("NEXT_TO_SWAP"), .fmask = F_NV40 },	// copies 330 [new channel] to 784 [channel used for ctx RAM access]
-	{ 0x09, 0x1f, C("NEXT_TO_CURRENT"), .fmask = F_NV40 },	// movs new channel RAMIN address to current channel RAMIN address, basically where the real switch happens
-	{ 0x0a, 0x1f, C("SET_CONTEXT_POINTER"), .fmask = F_NV40 },	// copies scratch to 334
-	{ 0x0e, 0x1f, C("END"), .fmask = F_NV40 },
-	{ 0, 0, CMD },
+static struct insn tabcmd5[] = {
+	{ 0x04, 0x1f, C("BIND_CTX_DMA") },	// rebinds DMA object for ctx access
+	{ 0x05, 0x1f, C("BIND_CHANNEL") },	// copies 330 [new channel] to 784 [channel used for MMU accesses]
+	{ 0x06, 0x1f, C("SET_REG_POINTER") },	// copies scratch to 334
+	{ 0x07, 0x1f, C("SET_XFER_POINTER") },	// copies scratch to 33c, anding it with 0xffff8
+	{ 0x08, 0x1f, C("START_TIMER") },
+	{ 0x09, 0x1f, C("CLEAR_RESET") },
+	{ 0x0a, 0x1f, C("HALT") },
+	{ 0x0b, 0x1f, C("INTR") },
+	{ 0x0c, 0x1f, C("END") },
+	{ 0x0d, 0x1f, C("FINISH_CHSW") },
+	// 0x11 seen
+	// 0x16 seen on G200+
+	{ 0, 0, OOPS, CMD },
+};
+
+static struct insn tabxarea4[] = {
+	{ 0x000001, 0x000007, N("main") },
+	{ 0x000002, 0x000007, N("unk2") },
+	{ 0, 0, OOPS },
+};
+
+static struct insn tabxarea5[] = {
+	{ 0x000000, 0x080000, N("main") },
+	{ 0x080000, 0x080000, N("mp") },
+	{ 0, 0, OOPS },
 };
 
 static struct insn tabm[] = {
-	{ 0x100000, 0xff0000, N("ctx"), PGRAPH5, SR, .fmask = F_G80 },
-	{ 0x100000, 0xf00000, N("ctx"), PGRAPH5, GSIZE5, .fmask = F_G80 },
-	{ 0x100000, 0xffc000, N("ctx"), PGRAPH4, SR, .fmask = F_NV40 },
-	{ 0x100000, 0xf00000, N("ctx"), PGRAPH4, GSIZE4, .fmask = F_NV40 },
-	{ 0x200000, 0xf00000, N("lsr"), IMM },			// moves 20-bit immediate to scratch reg
-	{ 0x300000, 0xf00000, N("lsr2"), IMM },			// moves 20-bit immediate to 338
-	{ 0x400000, 0xfc0000, N("jmp"), T(pred), BTARG },		// jumps if condition true
+	{ 0x000000, 0xf00000, N("nop") },
+	{ 0x100000, 0xff0000, N("reg"), PGRAPH5, RA, .fmask = F_G80 },
+	{ 0x100000, 0xf00000, N("reg"), PGRAPH5, GSIZE5, .fmask = F_G80 },
+	{ 0x100000, 0xffc000, N("reg"), PGRAPH4, RA, .fmask = F_NV40 },
+	{ 0x100000, 0xf00000, N("reg"), PGRAPH4, GSIZE4, .fmask = F_NV40 },
+	{ 0x200000, 0xf00000, N("li"), RA, IMM },				// moves 20-bit immediate to $ra
+	{ 0x300000, 0xf00000, N("li"), RB, IMM },				// moves 20-bit immediate to $rb
+	{ 0x400000, 0xfc0000, N("jmp"), T(pred), BTARG },			// jumps if condition true
 	{ 0x440000, 0xfc0000, N("call"), T(pred), CTARG, .fmask = F_CALLRET },	// calls if condition true
 	{ 0x480000, 0xfc0000, N("ret"), T(pred), .fmask = F_CALLRET },		// rets if condition true
-	{ 0x500000, 0xf00000, N("waitfor"), T(pred) },		// waits until condition true.
+	{ 0x500000, 0xf00000, N("waitfor"), T(pred) },				// waits until condition true.
 	{ 0x600000, 0xf00000, N("cmd"), T(cmd5), .fmask = F_G80 },		// runs a CMD.
 	{ 0x600000, 0xf00000, N("cmd"), T(cmd4), .fmask = F_NV40 },		// runs a CMD.
-	{ 0x700000, 0xf00080, N("clear"), T(rpred) },		// clears given flag
-	{ 0x700080, 0xf00080, N("set"), T(rpred) },		// sets given flag
-	{ 0x800000, 0xf80000, N("xfer1"), T(area), .fmask = F_G80 },
-	{ 0x880000, 0xf80000, N("xfer2"), T(area), .fmask = F_G80 },
-	{ 0x900000, 0xff0000, N("disable"), DIS0, .fmask = F_G80 },		// ors 0x40 with given immediate.
-	{ 0x910000, 0xff0000, N("disable"), DIS1, .fmask = F_G80 },
-	{ 0xa00000, 0xf00000, N("fl3"), PGRAPH5, .fmask = F_G80 },		// movs given PGRAPH register to 0x400830.
-	{ 0xc00000, 0xf80000, N("seek1"), T(area), SEEKP, .fmask = F_G80 },
-	{ 0xc80000, 0xf80000, N("seek2"), T(area), SEEKP, .fmask = F_G80 },
+	{ 0x700000, 0xf00080, N("clear"), T(rpred) },				// clears given flag
+	{ 0x700080, 0xf00080, N("set"), T(rpred) },				// sets given flag
+	{ 0x800000, 0xf00000, N("xfer"), T(xarea4), XSTART4, .fmask = F_NV40 },
+	{ 0x800000, 0xf00000, N("xfer"), T(xarea5), XMASK, .fmask = F_G80 },
+	{ 0x900000, 0xf00000, N("reset"), RIDX, RMASK },			// ors 0x40 with given immediate.
+	{ 0xa00000, 0xf00000, N("ldsr"), PGRAPH5, .fmask = F_G80 },		// movs given PGRAPH register to 0x400830 aka the scratch reg.
+	{ 0xc00000, 0xf00000, N("seek"), T(xarea5), XMASK, XSTART5, .fmask = F_G80 },
 	{ 0, 0, OOPS },
 };
 
