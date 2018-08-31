212a62aafada0e2d294996b23c0f02aaea623bd0
Rhys Kidd <rhyskidd@gmail.com>
2018-08-31 08:28:43 -0400

    demmt: Support VOLTA_CHANNEL_GPFIFO_A

--- demmt/nvrm_decode_ioctl.c
+++ demmt/nvrm_decode_ioctl.c
@@ -376,6 +376,7 @@ struct nvrm_create_arg_decoder nvrm_create_arg_decoders[] =
 		_(NVRM_FIFO_IB_UNKA2, struct nvrm_create_fifo_ib, decode_create_fifo_ib),
 		_(NVRM_FIFO_IB_GM107, struct nvrm_create_fifo_ib, decode_create_fifo_ib),
 		_(NVRM_FIFO_IB_GP102, struct nvrm_create_fifo_ib, decode_create_fifo_ib),
+		_(NVRM_FIFO_IB_GV100, struct nvrm_create_fifo_ib, decode_create_fifo_ib),
 		_(NVRM_CONTEXT_NEW, struct nvrm_create_context, decode_create_context),
 		_(NVRM_CONTEXT, struct nvrm_create_context, decode_create_context),
 		_(NVRM_EVENT, struct nvrm_create_event, decode_create_event),
