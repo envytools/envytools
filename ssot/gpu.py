envytools/envytools

commit 91a7f67e5ab4f70912a310bfda59a85a35a702ba

envytools: Support Turing TU116
TU116 is the engineering codename for the GPU dies shipping in the
GeForce GTX 1660 Ti.

ssot/gpu.py
@@ -1322,6 +1322,15 @@ class GpuTU106(GpuTU102):
     bios_major = 0x90
     bios_chip = 0x06
 
+class GpuTU116(GpuTU102):
+    id = 0x176
+    pciid = 0x2180
+    gpc_count = 3
+    tpc_count = 4
+    date = "22.02.2019"
+    bios_major = 0x90
+    bios_chip = 0x16
+
 
 from ssot.cgen import CGenerator, CPartEnum, CPartStruct, StructName
 from ssot.bus import cgen as cgen_bus

nvbios/info.c
@@ -333,6 +333,11 @@ int envy_bios_parse_bit_i (struct envy_bios *bios, struct envy_bios_bit_entry *b
 			bios->chipset = 0x166;
 			bios->chipset_name = "TU106";
 			break;
+		/* TU116 */
+		case 0x9016:
+			bios->chipset = 0x176;
+			bios->chipset_name = "TU116";
+			break;
 		default:
 			ENVY_BIOS_ERR("Unknown chipset detected: %x\n", info->version[0] << 8 | info->version[1]);
 			break;

commit 665e96da7410c4166575a87c859df6f50cefdc15

docs: Complete HDA documentation for TU116
Fixes: 91a7f67e ("envytools: Support Turing TU116")

ssot/gpu.py
@@ -1325,6 +1325,7 @@ class GpuTU106(GpuTU102):
 class GpuTU116(GpuTU102):
     id = 0x176
     pciid = 0x2180
+    hda_pciid = 0x1aeb
     gpc_count = 3
     tpc_count = 4
     date = "22.02.2019"