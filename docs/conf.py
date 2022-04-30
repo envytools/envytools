e11d670a70ae0455261ead53cdd09c321974cc64
Marcelina Kościelnicka <mwk@0x04.net>
2022-04-30 23:15:56 +0200

    fix standalone docs build

--- docs/conf.py
+++ docs/conf.py
@@ -94,7 +94,7 @@ primary_domain = 'envy'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'classic'
-html_style = '/classic.css'
+html_style = 'classic.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
